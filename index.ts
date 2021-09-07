15d069f872f7d8f877a3bda5cf067822ab222a5f
eyousefifar <eyousefifar@gmail.com>
2021-09-07 22:48:03 +0430

    initialize storage

--- index.ts
+++ index.ts
@@ -4,14 +4,22 @@ import {
   initDb as initAuthnzDb,
   initSecret as initAuthnzSecret,
 } from "./app/authnz";
+import {
+  initDb as initStorageDb,
+  initPublicBucket,
+  startStorageServer,
+} from "./app/storage";
 const app = httpClient({ dev: true });
 
 export async function startService() {
   try {
     await initAuthnzDb();
     await initAuthnzSecret();
+    await initStorageDb();
+    await initPublicBucket();
     startAuthnzServer(app);
-    await app.listen(3000, "0.0.0.0")
+    startStorageServer(app);
+    await app.listen(3000, "0.0.0.0");
   } catch (error) {
     console.log(error);
     process.exit(1);
