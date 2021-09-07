3af7f9718bb3f0ea1d6dd5e2eaa38c45b54ef104
eyousefifar <eyousefifar@gmail.com>
2021-09-07 21:50:40 +0430

    removed data policy and unused error path

--- app/storage/adapters/minio/minioClient.ts
+++ app/storage/adapters/minio/minioClient.ts
@@ -3,7 +3,6 @@ import { adapterTypes } from "../../types";
 
 export function buildMinioClient(args: adapterTypes.IBuildMinioClient) {
   const { Client } = args;
-  const errorPath = "storage service, adapters, upload to minio";
   const region = "est-ir";
   const endPoint = "127.0.0.1";
   const port = 9000;
@@ -15,44 +14,6 @@ export function buildMinioClient(args: adapterTypes.IBuildMinioClient) {
     useSSL: false,
     region,
   });
-
-  // const imagesPolicy = `
-  // {
-  //   "Version": "2012-10-17",
-  //   "Statement": [
-  //     {
-  //       "Action": [
-  //         "s3:GetBucketLocation",
-  //         "s3:ListBucket"
-  //       ],
-  //       "Effect": "Allow",
-  //       "Principal": {
-  //         "AWS": [
-  //           "*"
-  //         ]
-  //       },
-  //       "Resource": [
-  //         "arn:aws:s3:::images"
-  //       ],
-  //       "Sid": ""
-  //     },
-  //     {
-  //       "Action": [
-  //         "s3:GetObject"
-  //       ],
-  //       "Effect": "Allow",
-  //       "Principal": {
-  //         "AWS": [
-  //           "*"
-  //         ]
-  //       },
-  //       "Resource": [
-  //         "arn:aws:s3:::images/*"
-  //       ],
-  //       "Sid": ""
-  //     }
-  //   ]
-  // }`;
   return function minioClient(): Client {
     return client;
   };
