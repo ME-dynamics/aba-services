3aa92b0e0ac24e858a3b095c1c2ea199cadd0b32
eyousefifar <eyousefifar@gmail.com>
2021-09-07 16:39:12 +0430

    start taskyn service

--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
-// start server(s) here
+import { httpClient } from "aba-node";
+import {
+  startAuthnzServer,
+  initDb as initAuthnzDb,
+  initSecret as initAuthnzSecret,
+} from "./app/authnz";
+const app = httpClient({ dev: true });
+
+export async function startService() {
+  try {
+    await initAuthnzDb();
+    await initAuthnzSecret();
+    startAuthnzServer(app);
+    await app.listen(3000, "0.0.0.0")
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+startService().catch((err) => {
+  console.log(err);
+  process.exit(1);
+});
