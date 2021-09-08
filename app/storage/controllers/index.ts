4eba494a79dd257e25782112f109f452ff932305
eyousefifar <eyousefifar@gmail.com>
2021-09-08 23:08:54 +0430

    build and export private image

--- app/storage/controllers/index.ts
+++ app/storage/controllers/index.ts
@@ -1,6 +1,11 @@
 import { buildPostUploadImage } from "./postUploadImage";
+import { buildGetPrivateImage } from "./getPrivateImage";
 import { buildPostFileSession } from "./postFileSession";
+
+export const getPrivateImage = buildGetPrivateImage();
+
 
 export const postUploadImage = buildPostUploadImage();
 
 export const postFileSession = buildPostFileSession();
+
