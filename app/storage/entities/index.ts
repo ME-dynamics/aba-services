f5b0e80a7a571871348c870ce90c01d558af25c4
eyousefifar <eyousefifar@gmail.com>
2021-09-08 23:11:55 +0430

    inject server Url to image entities

--- app/storage/entities/index.ts
+++ app/storage/entities/index.ts
@@ -1,9 +1,9 @@
 // inject and export entities
 import { nanoid } from "nanoid";
 import { v4 } from "uuid";
-
+import { serverUrl } from "../config";
 import { buildMakeFileSession } from "./fileSession";
 import { buildMakeImage } from "./image";
 
 export const makeFileSession = buildMakeFileSession({ nanoid });
-export const makeImage = buildMakeImage({ uuid: v4 });
+export const makeImage = buildMakeImage({ uuid: v4, serverUrl });
