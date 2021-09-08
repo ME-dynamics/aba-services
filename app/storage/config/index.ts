48651244119668be387aa7b84985573e01429da7
eyousefifar <eyousefifar@gmail.com>
2021-09-08 23:04:21 +0430

    add server url to configs

--- app/storage/config/index.ts
+++ app/storage/config/index.ts
@@ -12,7 +12,10 @@ if (process.env.NODE_ENV !== "production") {
   }
 }
 export const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
-export const scyllaContactPoint = process.env.SCYLLA_CONTACT_POINT ? process.env.SCYLLA_CONTACT_POINT : "127.0.1.1";
+export const scyllaContactPoint = process.env.SCYLLA_CONTACT_POINT
+  ? process.env.SCYLLA_CONTACT_POINT
+  : "127.0.1.1";
 
-
-
+export const serverUrl = process.env.SERVER_URL
+  ? process.env.SERVER_URL
+  : "https://127.0.0.1:13000";
