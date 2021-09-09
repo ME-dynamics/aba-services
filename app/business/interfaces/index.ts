071cc63bfb3150e834614bd60d6c18038b8d2c50
eyousefifar <eyousefifar@gmail.com>
2021-09-09 19:42:41 +0430

    added schemas to endpoints

--- app/business/interfaces/index.ts
+++ app/business/interfaces/index.ts
@@ -7,24 +7,58 @@ import { removeRequest } from "./removeRequest";
 import { removeCustomer } from "./removeCustomer";
 import { retrieveCustomers } from "./retrieveCustomers";
 import { retrieveRequests } from "./retrieveRequests";
-
+import {
+  sConfirmSchema,
+  sCreateRequest,
+  sRejectRequest,
+  sRemoveCustomer,
+  sRemoveRequest,
+  sRetrieveCustomers,
+  sRetrieveRequests,
+} from "../schemas";
 
 const app = httpClient({ dev: true });
 
 const customerEndpoint = `/v1/customer`;
 const providerEndpoint = "/v1/provider";
 
-app.post(`${providerEndpoint}/requests/confirm`, confirmRequest);
-app.post(`${customerEndpoint}/requests`, createRequest);
+app.post(
+  `${providerEndpoint}/requests/confirm`,
+  { schema: sConfirmSchema },
+  confirmRequest
+);
+app.post(
+  `${customerEndpoint}/requests`,
+  { schema: sCreateRequest },
+  createRequest
+);
 
+app.delete(
+  `${providerEndpoint}/requests/reject/:customerId`,
+  { schema: sRejectRequest },
+  rejectRequest
+);
+app.delete(
+  `${providerEndpoint}/customers/:customerId`,
+  { schema: sRemoveCustomer },
+  removeCustomer
+);
+app.delete(
+  `${customerEndpoint}/requests`,
+  { schema: sRemoveRequest },
+  removeRequest
+);
 
-app.delete(`${providerEndpoint}/requests/reject/:customerId`, rejectRequest);
-app.delete(`${providerEndpoint}/customers/:customerId`, removeCustomer);
-app.delete(`${customerEndpoint}/requests`, removeRequest);
-
-
-app.get(`${providerEndpoint}/customers`, retrieveCustomers);
-app.get(`${providerEndpoint}/requests`, retrieveRequests);
+app.get(
+  `${providerEndpoint}/customers`,
+  { schema: sRetrieveCustomers },
+  retrieveCustomers
+);
+app.get(
+  `${providerEndpoint}/requests`,
+  { schema: sRetrieveRequests },
+  retrieveRequests
+);
 export async function startServer() {
   try {
     await app.listen(3000);
