e43b13bf3c9fd79fcc4ed5bfa9e5b27933159335
eyousefifar <eyousefifar@gmail.com>
2021-09-29 21:15:45 +0330

    remove form structure from database

--- app/form/adapters/db/initDb.ts
+++ app/form/adapters/db/initDb.ts
@@ -6,58 +6,53 @@ export function buildInitDb(args: adapterTypes.IBuildInit) {
   const { init } = args;
   const errorPath = "form, adapters, init db";
   return async function initDb() {
-    const {
-      createTableQuery,
-      createTypeQuery,
-      createMaterialView,
-      selectQuery,
-      operators,
-    } = queryGen;
-    const choiceUDT = createTypeQuery({
-      name: "choice",
-      version: applicationVersion,
-      columns: [
-        { name: "label", type: "TEXT" },
-        { name: "value", type: "TINYINT" },
-      ],
-    });
-    const questionUDT = createTypeQuery({
-      name: "question",
-      version: applicationVersion,
-      columns: [
-        { name: "question", type: "TEXT" },
-        {
-          name: "choices",
-          type: "SET",
-          setType: "UDT",
-          udtName: choiceUDT.entityName,
-        },
-      ],
-    });
-    const createFromStructureTable = createTableQuery({
-      name: "form_structure",
-      version: applicationVersion,
-      columns: [
-        { name: "id", type: "UUID" },
-        { name: "title", type: "TEXT" },
-        { name: "description", type: "TEXT" },
-        {
-          name: "fields",
-          type: "MAP",
-          map: {
-            keyType: "TEXT",
-            valueType: "UDT",
-            valueUdtName: questionUDT.entityName,
-          },
-        },
-        { name: "created_at", type: "TIMESTAMP" },
-        { name: "modified_at", type: "TIMESTAMP" },
-        { name: "soft_deleted", type: "BOOLEAN" },
-      ],
-      primaryKey: {
-        partition: ["id"],
-      },
-    });
+    const { createTableQuery, createMaterialView, selectQuery, operators } =
+      queryGen;
+    // const choiceUDT = createTypeQuery({
+    //   name: "choice",
+    //   version: applicationVersion,
+    //   columns: [
+    //     { name: "label", type: "TEXT" },
+    //     { name: "value", type: "TINYINT" },
+    //   ],
+    // });
+    // const questionUDT = createTypeQuery({
+    //   name: "question",
+    //   version: applicationVersion,
+    //   columns: [
+    //     { name: "question", type: "TEXT" },
+    //     {
+    //       name: "choices",
+    //       type: "SET",
+    //       setType: "UDT",
+    //       udtName: choiceUDT.entityName,
+    //     },
+    //   ],
+    // });
+    // const createFromStructureTable = createTableQuery({
+    //   name: "form_structure",
+    //   version: applicationVersion,
+    //   columns: [
+    //     { name: "id", type: "UUID" },
+    //     { name: "title", type: "TEXT" },
+    //     { name: "description", type: "TEXT" },
+    //     {
+    //       name: "fields",
+    //       type: "MAP",
+    //       map: {
+    //         keyType: "TEXT",
+    //         valueType: "UDT",
+    //         valueUdtName: questionUDT.entityName,
+    //       },
+    //     },
+    //     { name: "created_at", type: "TIMESTAMP" },
+    //     { name: "modified_at", type: "TIMESTAMP" },
+    //     { name: "soft_deleted", type: "BOOLEAN" },
+    //   ],
+    //   primaryKey: {
+    //     partition: ["id"],
+    //   },
+    // });
     const createFormDataTable = createTableQuery({
       name: "form_data",
       version: applicationVersion,
@@ -131,9 +126,9 @@ export function buildInitDb(args: adapterTypes.IBuildInit) {
         cluster: ["structure_id", "id"],
       },
     });
-    await init({ query: choiceUDT.query, errorPath });
-    await init({ query: questionUDT.query, errorPath });
-    await init({ query: createFromStructureTable.query, errorPath });
+    // await init({ query: choiceUDT.query, errorPath });
+    // await init({ query: questionUDT.query, errorPath });
+    // await init({ query: createFromStructureTable.query, errorPath });
     await init({ query: createFormDataTable.query, errorPath });
     await init({ query: createStructureIdMV.query, errorPath });
   };
