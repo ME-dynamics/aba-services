56b17368ca9681c7a0f3114588a8651355425bf6
eyousefifar <eyousefifar@gmail.com>
2021-10-02 13:03:22 +0330

    add business config

---
+++ app/business/config/index.ts
@@ -0,0 +1,16 @@
+if (process.env.NODE_ENV !== "production") {
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const { config } = require("dotenv");
+      config();
+    } catch (error) {
+      console.log(
+        "you should install dotenv in dev when not in production environment"
+      );
+      console.log(error);
+      process.exit(1);
+    }
+  }
+  
+  export const applicationName = "business";
+  export const applicationVersion = "v1";
