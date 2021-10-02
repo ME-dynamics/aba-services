0a6372207c74cb79a7132927a863c83735e6d93a
eyousefifar <eyousefifar@gmail.com>
2021-10-02 13:18:16 +0330

    neopir api

--- app/form/interfaces/index.ts
+++ app/form/interfaces/index.ts
@@ -1,11 +1,18 @@
-import { types } from "aba-node";
+import { types, routeGen } from "aba-node";
 import { sNEOPIRStructure } from "../schemas";
-
 import { retrieveNEOPIR } from "./retrieveNEOPIR";
-
+import { applicationVersion } from "../config";
 export async function startFormServer(app: types.tHttpInstance) {
   try {
-    app.get("/v1/form/neopir", { schema: sNEOPIRStructure }, retrieveNEOPIR);
+    app.get(
+      routeGen({
+        version: applicationVersion,
+        role: "shared",
+        routes: ["form", "neopir"],
+      }),
+      { schema: sNEOPIRStructure },
+      retrieveNEOPIR
+    );
   } catch (error) {
     console.log(error);
     process.exit(1);
--- app/form/interfaces/retrieveNEOPIR.ts
+++ app/form/interfaces/retrieveNEOPIR.ts
@@ -5,7 +5,7 @@ export function retrieveNEOPIR(request: any, reply: types.tReply) {
   try {
     const response = getNEOPIR();
     reply.code(response.code);
-    reply.send(reply);
+    reply.send(response);
   } catch (error) {
     reply.code(500);
     reply.send("unknown error");
