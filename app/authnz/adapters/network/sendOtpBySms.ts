28512623ca72a73ff5ec340e60f1696b1dff7330
eyousefifar <eyousefifar@gmail.com>
2021-10-08 00:00:36 +0330

    make send otp by sms network call based

--- app/authnz/adapters/network/sendOtpBySms.ts
+++ app/authnz/adapters/network/sendOtpBySms.ts
@@ -1,15 +1,23 @@
 import { adaptersTypes } from "../../types";
 
-export function buildSendOtpBySms() {
-  // need to inject http request by fetch;
-  // or later grpc for service
-  // need insert to db function
-  // insert to db query
-  return async function sendOtpBySms(info: adaptersTypes.ISendOtpSms): Promise<boolean> {
-    const { otpCode, phoneNumber } = info;
-    // make a request to sms service;
-    // store information, id maybe
-    console.log(`${phoneNumber} code is: ${otpCode} `);
-    return true;
-  };
+// export function buildSendOtpBySms() {
+//   // need to inject http request by fetch;
+//   // or later grpc for service
+//   // need insert to db function
+//   // insert to db query
+//   return async function sendOtpBySms(info: adaptersTypes.ISendOtpSms): Promise<boolean> {
+//     const { otpCode, phoneNumber } = info;
+//     // make a request to sms service;
+//     // store information, id maybe
+//     console.log(`${phoneNumber} code is: ${otpCode} `);
+//     return true;
+//   };
+// }
+
+export async function fetchSendOtpBySms(info: adaptersTypes.ISendOtpSms) {
+  const { otpCode, phoneNumber } = info;
+  // make a request to sms service;
+  // store information, id maybe
+  console.log(`${phoneNumber} code is: ${otpCode} `);
+  return true;
 }
