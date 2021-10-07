7a13f4b4328555e282cdb5302b00c98577588b50
eyousefifar <eyousefifar@gmail.com>
2021-10-07 16:32:57 +0330

    add image validation, change to owner id

--- app/note/usecases/updateNote.ts
+++ app/note/usecases/updateNote.ts
@@ -3,20 +3,24 @@ import { makeNote } from "../entities";
 import { entityTypes, usecaseTypes } from "../types";
 
 export function buildUpdateNote(args: usecaseTypes.IBuildUpdateNote) {
-  const { findNoteById, insertNote } = args;
+  const { findNoteById, insertNote, imageIdsValidation } = args;
   const { notFound, forbidden } = httpResultClientError;
   const { ok } = httpResultSuccess;
   return async function updateNote(info: usecaseTypes.IUpdateNote) {
-    const { id, userId, title, content, imageIds, imageToken } = info;
+    const { id, ownerId, title, content, imageIds } = info;
     const noteFound = await findNoteById(id);
     if (!noteFound || noteFound?.softDeleted) {
       return notFound({ error: "note not found" });
     }
-    if (noteFound.ownerId !== userId) {
+    // AUTHORIZE
+    if (noteFound.ownerId !== ownerId) {
       return forbidden({ error: "you don't have access to note" });
     }
-    if (imageToken) {
-      // request to get ids
+    if (imageIds) {
+      const imageValid = await imageIdsValidation(imageIds, ownerId);
+      if(!imageValid) {
+        return forbidden({ error: "image is not valid" });
+      } 
     }
     const note = makeNote(noteFound);
     note.set.title(title);
