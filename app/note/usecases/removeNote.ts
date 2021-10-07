8f2833693c761573e6b9b8d4bb5515a0734357db
eyousefifar <eyousefifar@gmail.com>
2021-10-07 16:32:24 +0330

    change to owner id

--- app/note/usecases/removeNote.ts
+++ app/note/usecases/removeNote.ts
@@ -7,12 +7,13 @@ export function buildRemoveNote(args: usecaseTypes.IBuildRemoveNote) {
   const { notFound, forbidden } = httpResultClientError;
   const { ok } = httpResultSuccess;
   return async function removeNote(info: usecaseTypes.IRemoveNote) {
-    const { userId, id } = info;
+    const { ownerId, id } = info;
     const noteFound = await findNoteById(id);
     if (!noteFound || noteFound?.softDeleted) {
       return notFound({ error: "note not found" });
     }
-    if (noteFound.ownerId !== userId) {
+    // AUTHORIZE
+    if (noteFound.ownerId !== ownerId) {
       return forbidden({ error: "you do not have access to this note" });
     }
     const note = makeNote(noteFound);
