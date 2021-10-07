b007cf2b125277059186c01d0f0ecda68a515b5c
eyousefifar <eyousefifar@gmail.com>
2021-10-07 16:33:12 +0330

    build and export use cases

--- app/note/usecases/index.ts
+++ app/note/usecases/index.ts
@@ -1,10 +1,19 @@
-import { insertNote, findUserNotes, findNoteById } from "../adapters";
+import {
+  insertNote,
+  findUserNotes,
+  findNoteById,
+  imageIdsValidation,
+} from "../adapters";
 import { buildCreateNote } from "./createNote";
 import { buildRetrieveUserNotes } from "./retrieveUserNotes";
 import { buildUpdateNote } from "./updateNote";
 import { buildRemoveNote } from "./removeNote";
 
-export const createNote = buildCreateNote({ insertNote });
+export const createNote = buildCreateNote({ insertNote, imageIdsValidation });
 export const retrieveUserNotes = buildRetrieveUserNotes({ findUserNotes });
-export const updateNote = buildUpdateNote({ findNoteById, insertNote });
+export const updateNote = buildUpdateNote({
+  findNoteById,
+  insertNote,
+  imageIdsValidation,
+});
 export const removeNote = buildRemoveNote({ findNoteById, insertNote });
