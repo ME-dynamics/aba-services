03c7258aac9ec21d95091fb60d78e91a38c5f972
eyousefifar <eyousefifar@gmail.com>
2021-10-07 16:32:13 +0330

    add image validation

--- app/note/usecases/createNote.ts
+++ app/note/usecases/createNote.ts
@@ -1,29 +1,33 @@
-import { httpResultSuccess } from "aba-node";
+import { httpResultSuccess, httpResultClientError } from "aba-node";
 import { makeNote } from "../entities";
 import { entityTypes, usecaseTypes } from "../types";
 
 export function buildCreateNote(args: usecaseTypes.IBuildCreateNote) {
-  const { insertNote } = args;
+  const { insertNote, imageIdsValidation } = args;
   const { created } = httpResultSuccess;
+  const { forbidden } = httpResultClientError;
   function noteInput(info: usecaseTypes.ICreateNote): entityTypes.IMakeNote {
-    const { ownerId, userId, title, content, imageToken } = info;
+    const { ownerId, userId, title, content, imageIds } = info;
     return {
       ownerId,
       userId,
       id: undefined,
       title,
       content,
-      imageIds: undefined,
+      imageIds,
       createdAt: undefined,
       modifiedAt: undefined,
       softDeleted: false,
     };
   }
   return async function createNote(info: usecaseTypes.ICreateNote) {
-    // get images if exists;
-    // create note
-    // insert note
-
+    const { imageIds, ownerId } = info;
+    if (imageIds) {
+      const imagesValid = await imageIdsValidation(imageIds, ownerId);
+      if (!imagesValid) {
+        return forbidden({ error: "image is not valid" });
+      }
+    }
     const note = makeNote(noteInput(info));
     await insertNote(note.object());
     return created<entityTypes.IMadeNoteObject>({
