1bc3de627a3e616ebd0973ab5424f9e989025ea7
eyousefifar <eyousefifar@gmail.com>
2021-10-07 16:30:08 +0330

    create note schema

---
+++ app/note/schemas/sCreateNote.ts
@@ -0,0 +1,43 @@
+import { fluentSchema, errorSchema, statusCodes } from "aba-node";
+
+const body = fluentSchema
+  .object()
+  .prop("userId", fluentSchema.string().format("uuid").required())
+  .prop("title", fluentSchema.string().required().maxLength(271))
+  .prop("content", fluentSchema.string().required().maxLength(9973))
+  .prop(
+    "imageIds",
+    fluentSchema
+      .array()
+      .items(fluentSchema.string().format("uuid").required())
+      .default(undefined)
+  );
+
+const response = {
+  [statusCodes.CREATED]: fluentSchema.object().prop(
+    "payload",
+    fluentSchema
+      .object()
+      .prop("ownerId", fluentSchema.string().format("uuid").required())
+      .prop("userId", fluentSchema.string().format("uuid").required())
+      .prop("id", fluentSchema.string().format("uuid").required())
+      .prop("title", fluentSchema.string().required().maxLength(271))
+      .prop("content", fluentSchema.string().required().maxLength(9973))
+      .prop(
+        "imageIds",
+        fluentSchema
+          .array()
+          .items(fluentSchema.string().format("uuid").required())
+          .default(null)
+      )
+      .prop("createdAt", fluentSchema.string().format("date").required())
+      .prop("modifiedAt", fluentSchema.string().format("date").required())
+  ).required(),
+  [statusCodes.INTERNAL_SERVER_ERROR]: errorSchema,
+  [statusCodes.FORBIDDEN]: errorSchema,
+};
+
+export const sCreateNote = {
+  body,
+  response,
+};
