4d4a49640bf3b8ec561067dc64cb0bc6179b1cd9
eyousefifar <eyousefifar@gmail.com>
2021-10-07 16:38:53 +0330

    remove file session use case

--- app/storage/usecases/createFileSession.ts
+++ app/storage/usecases/createFileSession.ts
@@ -1,39 +1,39 @@
-import { makeFileSession } from "../entities";
-import { httpResultSuccess, httpResultServerError } from "aba-node";
-import { entityTypes, usecaseTypes } from "../types";
+// import { makeFileSession } from "../entities";
+// import { httpResultSuccess, httpResultServerError } from "aba-node";
+// import { entityTypes, usecaseTypes } from "../types";
 
-export function buildCreateFileSession(
-  args: usecaseTypes.IBuildCreateFileSession
-) {
-  const { insertFileSession } = args;
-  const { created } = httpResultSuccess;
-  function fileSessionInput(
-    info: usecaseTypes.ICreateFileSession
-  ): entityTypes.IMakeFileSession {
-    const { userId, businessId, access, countLimit, sizeLimit } = info;
-    return {
-      session: undefined,
-      secret: undefined,
-      userId,
-      businessId,
-      access,
-      countLimit,
-      sizeLimit,
-      createdAt: undefined,
-      modifiedAt: undefined,
-      softDeleted: false,
-    };
-  }
-  return async function createFileSession(
-    info: usecaseTypes.ICreateFileSession
-  ) {
-    const fileSession = makeFileSession(fileSessionInput(info));
-    await insertFileSession(fileSession.object());
-    return created<usecaseTypes.IFileSessionResult>({
-      payload: {
-        session: fileSession.get.session(),
-        secret: fileSession.get.secret(),
-      },
-    });
-  };
-}
+// export function buildCreateFileSession(
+//   args: usecaseTypes.IBuildCreateFileSession
+// ) {
+//   const { insertFileSession } = args;
+//   const { created } = httpResultSuccess;
+//   function fileSessionInput(
+//     info: usecaseTypes.ICreateFileSession
+//   ): entityTypes.IMakeFileSession {
+//     const { userId, businessId, access, countLimit, sizeLimit } = info;
+//     return {
+//       session: undefined,
+//       secret: undefined,
+//       userId,
+//       businessId,
+//       access,
+//       countLimit,
+//       sizeLimit,
+//       createdAt: undefined,
+//       modifiedAt: undefined,
+//       softDeleted: false,
+//     };
+//   }
+//   return async function createFileSession(
+//     info: usecaseTypes.ICreateFileSession
+//   ) {
+//     const fileSession = makeFileSession(fileSessionInput(info));
+//     await insertFileSession(fileSession.object());
+//     return created<usecaseTypes.IFileSessionResult>({
+//       payload: {
+//         session: fileSession.get.session(),
+//         secret: fileSession.get.secret(),
+//       },
+//     });
+//   };
+// }
