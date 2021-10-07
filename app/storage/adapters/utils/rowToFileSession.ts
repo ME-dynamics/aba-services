4c09943a80a0cf2ef4d340bb9a0bed1a0ae59c8c
eyousefifar <eyousefifar@gmail.com>
2021-10-07 16:34:39 +0330

    remove file session utils

--- app/storage/adapters/utils/rowToFileSession.ts
+++ app/storage/adapters/utils/rowToFileSession.ts
@@ -1,19 +1,19 @@
-import { types } from "aba-node";
-import { entityTypes } from "../../types";
+// import { types } from "aba-node";
+// import { entityTypes } from "../../types";
 
-export function rowToFileSession(
-  row: types.tRow
-): entityTypes.IMadeFileSessionObject {
-  return {
-    session: row.get("session"),
-    secret: row.get("secret"),
-    userId: row.get("user_id").toString(),
-    businessId: row.get("business_id").toString(),
-    access: row.get("access"),
-    countLimit: row.get("count_limit"),
-    sizeLimit: row.get("size_limit"),
-    createdAt: row.get("created_at"),
-    modifiedAt: row.get("modified_at"),
-    softDeleted: row.get("soft_deleted"),
-  };
-}
+// export function rowToFileSession(
+//   row: types.tRow
+// ): entityTypes.IMadeFileSessionObject {
+//   return {
+//     session: row.get("session"),
+//     secret: row.get("secret"),
+//     userId: row.get("user_id").toString(),
+//     businessId: row.get("business_id").toString(),
+//     access: row.get("access"),
+//     countLimit: row.get("count_limit"),
+//     sizeLimit: row.get("size_limit"),
+//     createdAt: row.get("created_at"),
+//     modifiedAt: row.get("modified_at"),
+//     softDeleted: row.get("soft_deleted"),
+//   };
+// }
