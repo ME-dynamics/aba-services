97398a96f0e3b2eef9ffb747369377b4b5c84c0a
eyousefifar <eyousefifar@gmail.com>
2021-10-07 16:31:04 +0330

    export schemas

---
+++ app/note/schemas/index.ts
@@ -0,0 +1,4 @@
+export { sCreateNote } from "./sCreateNote";
+export { sUpdateNote } from "./sUpdateNote";
+export { sRetrieveUserNotes } from "./sRetrieveUserNotes";
+export { sRemoveNote } from "./sRemoveNote";
