57e17b7c6025620a885b54e93baa386008dd14d5
eyousefifar <eyousefifar@gmail.com>
2021-10-08 00:00:14 +0330

    connect to user service, create user function

--- app/authnz/adapters/network/createUser.ts
+++ app/authnz/adapters/network/createUser.ts
@@ -1,14 +1,12 @@
-
-// ! temp id generator ==> remove later
-import { v4 } from "uuid"
+import { createUser } from "../../../user";
 
 /**
  * sync request to user service, using http or gRPC
  * create a user on user verification
  * @returns {string} user id
  */
-export function buildCreateUser() {
-  return async function createUser(otpId: string) {
-      return true
-  };
+export async function fetchCreateUser(userId: string, phoneNumber: string) {
+  const user = await createUser({ id: userId, phoneNumber });
+  const { username } = user.payload;
+  return username
 }
