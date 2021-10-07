d8ec2fb0ed2cabe574fef46408c35cf67551c0f9
eyousefifar <eyousefifar@gmail.com>
2021-10-07 21:33:05 +0330

    add transform, fix validation

--- app/storage/controllers/postUploadImage.ts
+++ app/storage/controllers/postUploadImage.ts
@@ -1,6 +1,7 @@
 import { httpResultClientError, auth, types } from "aba-node";
 import { controllerTypes } from "../types";
 import { uploadImage } from "../usecases";
+import { v4 } from "uuid";
 export function buildPostUploadImage() {
   const roles: types.IRoles = {
     admin: true,
@@ -10,47 +11,80 @@ export function buildPostUploadImage() {
     assistant: true,
     support: true,
   };
-  const { badRequest, unauthorized } = httpResultClientError;
-  function validate(fileData: controllerTypes.tMultiPartFile) {
-    if (!fileData.fields.session) {
+  const { badRequest, forbidden } = httpResultClientError;
+  function validate(fileData: controllerTypes.tMultiPartFile):
+    | {
+        access: "private" | "public";
+        transform: "profile" | "note";
+      }
+    | false {
+    if (!fileData.fields.access) {
       return false;
     }
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
-    const { fieldname, value } = fileData.fields.session;
+    const accessValue = fileData.fields?.access?.value;
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const transformValue = fileData.fields?.transform?.value;
     const keys = Object.keys(fileData.fields);
-    if (keys.length < 2 || keys.length > 2) {
+    if (keys.length !== 3) {
       return false;
     }
-    if (keys[0] !== "session" || keys[1] !== "imageFile") {
+    if (
+      keys[0] !== "access" ||
+      keys[1] !== "transform" ||
+      keys[2] !== "imageFile"
+    ) {
       return false;
     }
-    if (fieldname !== "session") {
+
+    if (!accessValue || typeof accessValue !== "string") {
       return false;
     }
-    if (!value || typeof value !== "string") {
+    if (!transformValue || typeof transformValue !== "string") {
       return false;
     }
-    return value;
+    if (accessValue === "private" && transformValue === "note") {
+      return {
+        access: "private",
+        transform: "note",
+      };
+    }
+    if (accessValue === "public" && transformValue === "profile") {
+      return {
+        access: "public",
+        transform: "profile",
+      };
+    }
+    return false;
   }
   return async function postUploadImage(
     httpRequest: controllerTypes.tPostUploadImage
   ) {
-    const { success, error, payload } = auth(httpRequest, roles);
-    if (!success) {
-      return error;
-    }
+    // const { success, error, payload } = auth(httpRequest, roles);
+    // if (!success) {
+    //   return error;
+    // }
     const fileData = await httpRequest.file();
-    const session = validate(fileData);
-    if (session) {
+    fileData.file.on("limit", function onFileLimit() {
+      return forbidden({ error: "file is too large" });
+    });
+    const isValid = validate(fileData);
+    if (isValid) {
+      const { access, transform } = isValid;
       const result = await uploadImage({
         file: fileData.file,
-        userId: payload.userId,
-        session,
+        userId: v4(),
+        access,
+        transform
       });
+
       return result;
     }
     fileData.file.unpipe();
-    return badRequest({ error: "session should exists" });
+    return badRequest({
+      error: "you should define access type and image file",
+    });
   };
 }
