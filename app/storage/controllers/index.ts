7960defeb7fdfb83b38f3b40ab2f4a3be3df4098
eyousefifar <eyousefifar@gmail.com>
2021-10-07 16:35:54 +0330

    remove file session controller

--- app/storage/controllers/index.ts
+++ app/storage/controllers/index.ts
@@ -1,11 +1,11 @@
 import { buildPostUploadImage } from "./postUploadImage";
 import { buildGetPrivateImage } from "./getPrivateImage";
-import { buildPostFileSession } from "./postFileSession";
+// import { buildPostFileSession } from "./postFileSession";
 
 export const getPrivateImage = buildGetPrivateImage();
 
 
 export const postUploadImage = buildPostUploadImage();
 
-export const postFileSession = buildPostFileSession();
+// export const postFileSession = buildPostFileSession();
 
