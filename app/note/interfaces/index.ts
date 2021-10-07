7d8050ecf6f2523e092a6c9ac29e709164ba8538
eyousefifar <eyousefifar@gmail.com>
2021-10-07 16:29:57 +0330

    notes routes

--- app/note/interfaces/index.ts
+++ app/note/interfaces/index.ts
@@ -1,1 +1,43 @@
-// start your http, grpc, kafka , etc interface here
+import { types, routeGen } from "aba-node";
+
+import { retrieveUserNotes } from "./retrieveUserNotes";
+import { createNote } from "./createNote";
+import { updateNote } from "./updateNote";
+import { removeNote } from "./removeNote";
+import { applicationVersion } from "../config";
+
+export function startNoteServer(app: types.tHttpInstance) {
+  app.get(
+    routeGen({
+      version: applicationVersion,
+      role: "provider",
+      routes: ["notes", ":userId"],
+    }),
+    retrieveUserNotes
+  );
+
+  app.post(
+    routeGen({
+      version: applicationVersion,
+      role: "provider",
+      routes: ["notes"],
+    }),
+    createNote
+  );
+  app.put(
+    routeGen({
+      version: applicationVersion,
+      role: "provider",
+      routes: ["notes"],
+    }),
+    updateNote
+  );
+  app.delete(
+    routeGen({
+      version: applicationVersion,
+      role: "provider",
+      routes: ["notes", ":noteId"],
+    }),
+    removeNote
+  );
+}
