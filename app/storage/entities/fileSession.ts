9b5f9c8b33e3369cae59949f637e783be5c6142d
eyousefifar <eyousefifar@gmail.com>
2021-10-07 16:36:55 +0330

    remove file session entities

--- app/storage/entities/fileSession.ts
+++ app/storage/entities/fileSession.ts
@@ -1,61 +1,61 @@
-import { entityTypes } from "../types";
-export function buildMakeFileSession(args: entityTypes.IBuildMakeFileSession) {
-  const { nanoid } = args;
-  return function makeFileSession(image: entityTypes.IMakeFileSession) {
-    const {
-      session = nanoid(32),
-      secret = nanoid(64),
-      userId,
-      businessId,
-      access,
-      countLimit = 1,
-      sizeLimit = 5, // in megabytes
-      createdAt = new Date(),
-      modifiedAt = new Date(),
-    } = image;
-    let { softDeleted } = image;
-    // * Setters
-    function remove() {
-      softDeleted = true;
-      modifiedAt.setTime(Date.now());
-    }
-    function restore() {
-      softDeleted = false;
-      modifiedAt.setTime(Date.now());
-    }
-    const madeFileSession: entityTypes.IMadeFileSession = {
-      get: {
-        session: () => session,
-        secret: () => secret,
-        userId: () => userId,
-        businessId: () => businessId,
-        access: () => access,
-        countLimit: () => countLimit,
-        sizeLimit: () => sizeLimit,
-        createdAt: () => createdAt,
-        modifiedAt: () => modifiedAt,
-        softDeleted: () => softDeleted,
-      },
-      set: {
-        remove,
-        restore,
-      },
-      object: () => {
-        return {
-          session,
-          secret,
-          userId,
-          businessId,
-          access,
-          countLimit,
-          sizeLimit,
-          createdAt,
-          modifiedAt,
-          softDeleted,
-        };
-      },
-    };
+// import { entityTypes } from "../types";
+// export function buildMakeFileSession(args: entityTypes.IBuildMakeFileSession) {
+//   const { nanoid } = args;
+//   return function makeFileSession(image: entityTypes.IMakeFileSession) {
+//     const {
+//       session = nanoid(32),
+//       secret = nanoid(64),
+//       userId,
+//       businessId,
+//       access,
+//       countLimit = 1,
+//       sizeLimit = 5, // in megabytes
+//       createdAt = new Date(),
+//       modifiedAt = new Date(),
+//     } = image;
+//     let { softDeleted } = image;
+//     // * Setters
+//     function remove() {
+//       softDeleted = true;
+//       modifiedAt.setTime(Date.now());
+//     }
+//     function restore() {
+//       softDeleted = false;
+//       modifiedAt.setTime(Date.now());
+//     }
+//     const madeFileSession: entityTypes.IMadeFileSession = {
+//       get: {
+//         session: () => session,
+//         secret: () => secret,
+//         userId: () => userId,
+//         businessId: () => businessId,
+//         access: () => access,
+//         countLimit: () => countLimit,
+//         sizeLimit: () => sizeLimit,
+//         createdAt: () => createdAt,
+//         modifiedAt: () => modifiedAt,
+//         softDeleted: () => softDeleted,
+//       },
+//       set: {
+//         remove,
+//         restore,
+//       },
+//       object: () => {
+//         return {
+//           session,
+//           secret,
+//           userId,
+//           businessId,
+//           access,
+//           countLimit,
+//           sizeLimit,
+//           createdAt,
+//           modifiedAt,
+//           softDeleted,
+//         };
+//       },
+//     };
 
-    return madeFileSession;
-  };
-}
+//     return madeFileSession;
+//   };
+// }
--- app/storage/entities/index.ts
+++ app/storage/entities/index.ts
@@ -2,8 +2,8 @@
 import { nanoid } from "nanoid";
 import { v4 } from "uuid";
 import { serverUrl } from "../config";
-import { buildMakeFileSession } from "./fileSession";
+// import { buildMakeFileSession } from "./fileSession";
 import { buildMakeImage } from "./image";
 
-export const makeFileSession = buildMakeFileSession({ nanoid });
+// export const makeFileSession = buildMakeFileSession({ nanoid });
 export const makeImage = buildMakeImage({ uuid: v4, serverUrl });
