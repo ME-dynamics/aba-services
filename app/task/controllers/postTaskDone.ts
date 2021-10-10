bbc13b9b5da7fedbca47a87a9995cb9a807b7b36
eyousefifar <eyousefifar@gmail.com>
2021-10-10 17:35:53 +0330

    task done controller with authnz

---
+++ app/task/controllers/postTaskDone.ts
@@ -0,0 +1,43 @@
+import { auth, types, httpResultClientError } from "aba-node";
+import { taskDone } from "../usecases";
+import { controllerTypes } from "../types";
+export function buildPostTaskDone(args: controllerTypes.IBuildPostTaskDone) {
+  const { findUserProvider } = args;
+  const roles: types.IRoles = {
+    customer: true,
+    provider: true,
+    accountant: false,
+    admin: true,
+    assistant: false,
+    support: false,
+  };
+  const { badRequest, forbidden } = httpResultClientError;
+  return async function postTaskDone(
+    httpRequest: controllerTypes.tPostTaskDone
+  ) {
+    const { success, error, payload } = auth(httpRequest, roles);
+    if (!success) {
+      return error;
+    }
+    const { taskId, userId } = httpRequest.body;
+    const { role } = payload;
+    if (role === "admin") {
+      if (!userId) {
+        return badRequest({ error: "user id must be defined" });
+      }
+      return await taskDone({ taskId, userId });
+    }
+    if (role === "provider") {
+      const providerId = payload.userId;
+      if (!userId) {
+        return badRequest({ error: "user id must be defined" });
+      }
+      const userProviderId = await findUserProvider(userId);
+      if (!userProviderId || userProviderId !== providerId) {
+        return forbidden({ error: "action not allowed" });
+      }
+      return await taskDone({ taskId, userId });
+    }
+    return await taskDone({ taskId, userId: payload.userId });
+  };
+}
