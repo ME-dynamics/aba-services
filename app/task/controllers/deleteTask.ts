701b9c90362de9f21a6f6daaa9b6ff1c5b3d0ec3
eyousefifar <eyousefifar@gmail.com>
2021-10-10 17:35:20 +0330

    delete task controller with authnz

---
+++ app/task/controllers/deleteTask.ts
@@ -0,0 +1,41 @@
+import { auth, types, httpResultClientError } from "aba-node";
+import { removeTask } from "../usecases";
+import { controllerTypes } from "../types";
+export function buildDeleteTask(args: controllerTypes.IBuildRemoveTask) {
+  const { findUserProvider } = args;
+  const roles: types.IRoles = {
+    customer: true,
+    provider: true,
+    accountant: false,
+    admin: true,
+    assistant: false,
+    support: false,
+  };
+  const { badRequest, forbidden } = httpResultClientError;
+  return async function deleteTask(httpRequest: controllerTypes.tDeleteTask) {
+    const { success, error, payload } = auth(httpRequest, roles);
+    if (!success) {
+      return error;
+    }
+    const { taskId, userId } = httpRequest.params;
+    const { role } = payload;
+    if (role === "admin") {
+      if (!userId) {
+        return badRequest({ error: "user id must be defined" });
+      }
+      return await removeTask({ taskId, userId });
+    }
+    if (role === "provider") {
+      const providerId = payload.userId;
+      if (!userId) {
+        return badRequest({ error: "user id must be defined" });
+      }
+      const userProviderId = await findUserProvider(userId);
+      if (!userProviderId || userProviderId !== providerId) {
+        return forbidden({ error: "action not allowed" });
+      }
+      return await removeTask({ taskId, userId });
+    }
+    return await removeTask({ taskId, userId: payload.userId });
+  };
+}
