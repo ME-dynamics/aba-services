a298cc2190888b0eab550edfe9fce8ffa0581f60
eyousefifar <eyousefifar@gmail.com>
2021-10-16 11:53:20 +0330

    update and create patient

--- app/user/usecases/createPatient.ts
+++ app/user/usecases/createPatient.ts
@@ -3,9 +3,9 @@ import { makePatient } from "../entities";
 import { usecaseTypes, entityTypes } from "../types";
 
 export function buildCreatePatient(args: usecaseTypes.IBuildCreatePatient) {
-  const { findUserById, insertPatient } = args;
+  const { findUserById, findPatientByUserId, insertPatient } = args;
   const { notFound } = httpResultClientError;
-  const { ok } = httpResultSuccess;
+  const { created, ok } = httpResultSuccess;
   function patientInput(
     info: usecaseTypes.tCreatePatient
   ): entityTypes.IPatient {
@@ -56,12 +56,20 @@ export function buildCreatePatient(args: usecaseTypes.IBuildCreatePatient) {
     };
   }
   return async function createPatient(info: usecaseTypes.tCreatePatient) {
-    const userFound = await findUserById(info.userId);
+    const { userId } = info;
+    const userFound = await findUserById(userId);
     if (!userFound || userFound.softDeleted) {
       return notFound({ error: "user not found" });
     }
+    const patientFound = await findPatientByUserId(userId);
     const patient = makePatient(patientInput(info));
     await insertPatient(patient.object());
+
+    if (!patientFound || patientFound.softDeleted) {
+      return created({
+        payload: patient.object(),
+      });
+    }
     return ok({
       payload: patient.object(),
     });
