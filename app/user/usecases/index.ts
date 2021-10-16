6402ef854c9dbd76cdcd7c856d21cfc542a12704
eyousefifar <eyousefifar@gmail.com>
2021-10-16 11:53:52 +0330

    build and export usecases

--- app/user/usecases/index.ts
+++ app/user/usecases/index.ts
@@ -1,12 +1,26 @@
-import { findUserById, findUserByPhoneNumber, insertUser } from "../adapters"
+import {
+  findUserById,
+  findUserByPhoneNumber,
+  insertUser,
+  findPatientByUserId,
+  insertPatient,
+} from "../adapters";
 
 import { buildCreateUser } from "./createUser";
 import { buildRetrieveUser } from "./retrieveUser";
+import { buildUpdateUser } from "./updateUser";
+import { buildCreatePatient } from "./createPatient";
+import { buildRetrievePatient } from "./retrievePatient";
 
-
-
-export const retrieveUser = buildRetrieveUser({findUserById})
-
-
-
-export const createUser = buildCreateUser({findUserByPhoneNumber, insertUser});
+export const retrieveUser = buildRetrieveUser({ findUserById });
+export const createUser = buildCreateUser({
+  findUserByPhoneNumber,
+  insertUser,
+});
+export const retrievePatient = buildRetrievePatient({ findPatientByUserId });
+export const createPatient = buildCreatePatient({
+  findUserById,
+  findPatientByUserId,
+  insertPatient,
+});
+export const updateUser = buildUpdateUser({ findUserById, insertUser });
