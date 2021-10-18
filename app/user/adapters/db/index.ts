21a0519641435f52e4536b9287b49c15e2932a32
eyousefifar <eyousefifar@gmail.com>
2021-10-18 20:35:24 +0330

    export db func

--- app/user/adapters/db/index.ts
+++ app/user/adapters/db/index.ts
@@ -2,3 +2,5 @@ export { buildFindUserById } from "./findUserById";
 export { buildFindUserByPhoneNumber } from "./findUserByPhoneNumber";
 export { buildInitDb } from "./initDb";
 export { buildInsertUser } from "./insertUser"
+export { buildFindPatientByUserId } from "./findPatientByUserId"
+export { buildInsertPatient } from "./insertPatient"
