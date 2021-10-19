e28319fc38005f23d85d9ec3c3176da1445c2803
eyousefifar <eyousefifar@gmail.com>
2021-10-19 17:20:07 +0330

    inject fetch customer provider

--- app/user/controllers/index.ts
+++ app/user/controllers/index.ts
@@ -1,5 +1,6 @@
-import { buildGetRetrieveUser } from "./getRetrieveUser";
+import { fetchCustomerProvider } from "../adapters";
+import { buildGetUser } from "./getUser";
 import { buildPostCreateUser } from "./postCreateUser";
 
-export const getRetrieveUser = buildGetRetrieveUser();
+export const getUser = buildGetUser({ fetchCustomerProvider });
 export const postCreateUser = buildPostCreateUser();
