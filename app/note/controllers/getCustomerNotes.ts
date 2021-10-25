27c3823e092f916d6fae0ff361350d2e5394c2a8
eyousefifar <eyousefifar@gmail.com>
2021-10-25 19:12:48 +0330

    change retrieve user note to get customer note

---
+++ app/note/controllers/getCustomerNotes.ts
@@ -0,0 +1,34 @@
+import { auth, types, httpResultClientError } from "aba-node";
+import { retrieveUserNotes } from "../usecases";
+import { controllerTypes } from "../types";
+
+export function buildGetCustomerNotes(
+  args: controllerTypes.IBuildGetCustomerNotes
+) {
+  const { fetchCustomerProvider } = args;
+  const roles: types.IRoles = {
+    customer: false,
+    provider: true,
+    admin: false,
+    accountant: false,
+    assistant: false,
+    support: false,
+  };
+  const { forbidden } = httpResultClientError;
+  return async function getCustomerNotes(
+    httpRequest: controllerTypes.tGetCustomerNotes
+  ) {
+    const { success, error, payload } = auth(httpRequest, roles);
+    if (!success) {
+      return error;
+    }
+
+    const { userId } = payload;
+    const { id } = httpRequest.params;
+    const providerId = await fetchCustomerProvider(id);
+    if (!providerId || providerId !== userId) {
+      return forbidden({ error: "action not allowed" });
+    }
+    return await retrieveUserNotes({ providerId: userId, customerId: id });
+  };
+}
