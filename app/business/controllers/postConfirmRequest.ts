7f917ba7267eadc5dfdd5a5266453fb0b8a25118
eyousefifar <eyousefifar@gmail.com>
2021-10-29 20:32:47 +0330

    bug fix: user is not a customer before request request must be confirmed for user to become a customer

--- app/business/controllers/postConfirmRequest.ts
+++ app/business/controllers/postConfirmRequest.ts
@@ -1,6 +1,6 @@
-import { auth, types, httpResultClientError } from "aba-node"
+import { auth, types } from "aba-node";
 import { controllerTypes } from "../types";
-import { confirmRequest, retrieveCustomerProvider } from "../usecases";
+import { confirmRequest } from "../usecases";
 export function buildPostConfirmRequest() {
   const roles: types.IRoles = {
     customer: false,
@@ -9,21 +9,17 @@ export function buildPostConfirmRequest() {
     accountant: false,
     assistant: false,
     support: false,
-  }
-  const { forbidden } = httpResultClientError
+  };
   return async function postConfirmRequest(
     httpRequest: controllerTypes.tPostConfirmRequest
   ) {
     const { success, error, payload } = auth(httpRequest, roles);
-    if(!success) {
+    if (!success) {
       return error;
     }
     const { userId } = payload;
     const { customerId } = httpRequest.body;
-    const providerId = await retrieveCustomerProvider(customerId);
-    if(!providerId || providerId !== userId) {
-      return forbidden({error: "action not allowed"})
-    }
+
     return await confirmRequest({
       customerId,
       providerId: userId,
