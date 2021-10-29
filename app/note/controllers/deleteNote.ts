c66ac69c3e766b835a90be24e835b980c2a1eb2c
eyousefifar <eyousefifar@gmail.com>
2021-10-29 20:41:03 +0330

    fix auth bug

--- app/note/controllers/deleteNote.ts
+++ app/note/controllers/deleteNote.ts
@@ -1,9 +1,8 @@
-import { auth, types, httpResultClientError } from "aba-node";
+import { auth, types } from "aba-node";
 import { removeNote } from "../usecases";
 import { controllerTypes } from "../types";
 
 export function buildDeleteNote(args: controllerTypes.IBuildDeleteNote) {
-  const { fetchCustomerProvider } = args;
   const roles: types.IRoles = {
     customer: false,
     provider: true,
@@ -12,7 +11,6 @@ export function buildDeleteNote(args: controllerTypes.IBuildDeleteNote) {
     assistant: false,
     support: false,
   };
-  const { forbidden } = httpResultClientError;
   return async function deleteNote(httpRequest: controllerTypes.tDeleteNote) {
     const { success, error, payload } = auth(httpRequest, roles);
     if (!success) {
@@ -21,10 +19,7 @@ export function buildDeleteNote(args: controllerTypes.IBuildDeleteNote) {
 
     const { userId } = payload;
     const { noteId } = httpRequest.params;
-    const providerId = await fetchCustomerProvider(noteId);
-    if (!providerId || providerId !== userId) {
-      return forbidden({ error: "action not allowed" });
-    }
+
     return await removeNote({ providerId: userId, id: noteId });
   };
 }
