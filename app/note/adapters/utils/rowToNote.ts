f46938dac887bde56ce21dc3d4f235412bc8dc6e
eyousefifar <eyousefifar@gmail.com>
2021-10-29 20:40:33 +0330

    fix type conversion

--- app/note/adapters/utils/rowToNote.ts
+++ app/note/adapters/utils/rowToNote.ts
@@ -3,9 +3,9 @@ import { entityTypes } from "../../types";
 
 export function rowToNote(row: types.tRow): entityTypes.IMadeNoteObject {
   return {
-    providerId: row.get("provider_id"),
-    customerId: row.get("customer_id"),
-    id: row.get("id"),
+    providerId: row.get("provider_id").toString(),
+    customerId: row.get("customer_id").toString(),
+    id: row.get("id").toString(),
     title: row.get("title"),
     content: row.get("content"),
     imageIds: nullToUndefined<string[]>(row.get("image_ids")),
