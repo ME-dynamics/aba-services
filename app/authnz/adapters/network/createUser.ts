a5f82ea4beb6b0d6412dee1789d74ec277d0b703
eyousefifar <eyousefifar@gmail.com>
2021-11-10 20:53:00 +0330

    add role to create user function user service needs role now to sort out user types

--- app/authnz/adapters/network/createUser.ts
+++ app/authnz/adapters/network/createUser.ts
@@ -1,12 +1,14 @@
 import { createUser } from "../../../user";
-
+import { adaptersTypes } from "../../types";
 /**
  * sync request to user service, using http or gRPC
  * create a user on user verification
  * @returns {string} user id
  */
-export async function fetchCreateUser(userId: string, phoneNumber: string) {
-  const user = await createUser({ id: userId, phoneNumber });
+export async function fetchCreateUser(args: adaptersTypes.ICreateUser) {
+  const { userId, role, phoneNumber } = args;
+
+  const user = await createUser({ id: userId, role, phoneNumber });
   const { username } = user.payload;
-  return username
+  return username;
 }
