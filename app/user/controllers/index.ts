81fee11b91dbeb4c1bebdc8c30abc4fd3ed432c9
eyousefifar <eyousefifar@gmail.com>
2021-11-11 17:09:21 +0330

    build and exports controllers

--- app/user/controllers/index.ts
+++ app/user/controllers/index.ts
@@ -1,6 +1,23 @@
-import { fetchCustomerProvider } from "../adapters";
+import {
+  fetchCustomerProvider,
+  parseStoragePublicUrl,
+  fetchImageInfo,
+} from "../adapters";
 import { buildGetUser } from "./getUser";
 import { buildPostCreateUser } from "./postCreateUser";
+import { buildPutUpdateUser } from "./putUpdateUser";
+import { buildPostCreatePatient } from "./postCreatePatient";
+import { buildGetPatient } from "./getPatient";
+import { buildGetProviders } from "./getProviders";
 
 export const getUser = buildGetUser({ fetchCustomerProvider });
+export const getProviders = buildGetProviders();
 export const postCreateUser = buildPostCreateUser();
+export const putUpdateUser = buildPutUpdateUser({
+  fetchImageInfo,
+  parseStoragePublicUrl,
+});
+export const postCreatePatient = buildPostCreatePatient({
+  fetchCustomerProvider,
+});
+export const getPatient = buildGetPatient({ fetchCustomerProvider });
