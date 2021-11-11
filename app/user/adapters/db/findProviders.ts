3387425e66e09ae782dda06bc7092499e41f8dd7
eyousefifar <eyousefifar@gmail.com>
2021-11-11 17:06:05 +0330

    find providers implementation

---
+++ app/user/adapters/db/findProviders.ts
@@ -0,0 +1,44 @@
+import { queryGen } from "aba-node";
+import { adapterTypes, entityTypes } from "../../types";
+
+function selectQueryGen(): string {
+  const { selectQuery, operators } = queryGen;
+  const { equal } = operators;
+  const query = selectQuery({
+    table: "providers",
+    version: "v1",
+    columns: ["*"],
+    where: [equal({argument: "role", equals: "'provider'"})],
+  });
+  return query;
+}
+
+export function buildFindProviders(args: adapterTypes.IBuildFindProviders) {
+  const { select, rowToUser } = args;
+  const errorPath = "user, adapters, db, find providers";
+  const query = selectQueryGen();
+  return async function findProviders(): Promise<
+    entityTypes.IMadeUserObject[] | undefined
+  > {
+    const result = await select({
+      query,
+      params: undefined,
+      unique: false,
+      queryOptions: undefined,
+      errorPath,
+    });
+    const length = result.rowLength;
+    if (length === 0) {
+      return undefined;
+    }
+    const providers: entityTypes.IMadeUserObject[] = [];
+    for (let index = 0; index < length; index++) {
+      const row = result.rows[index];
+      if (row.get("soft_deleted")) {
+        continue;
+      }
+      providers.push(rowToUser(row));
+    }
+    return providers;
+  };
+}
