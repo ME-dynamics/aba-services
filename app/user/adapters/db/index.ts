0f741cbf08140e83af8efc7384124aad444ca24f
eyousefifar <eyousefifar@gmail.com>
2021-11-11 17:07:37 +0330

    export find providers

--- app/user/adapters/db/index.ts
+++ app/user/adapters/db/index.ts
@@ -1,6 +1,7 @@
 export { buildFindUserById } from "./findUserById";
 export { buildFindUserByPhoneNumber } from "./findUserByPhoneNumber";
 export { buildInitDb } from "./initDb";
-export { buildInsertUser } from "./insertUser"
-export { buildFindPatientByUserId } from "./findPatientByUserId"
-export { buildInsertPatient } from "./insertPatient"
+export { buildInsertUser } from "./insertUser";
+export { buildFindPatientByUserId } from "./findPatientByUserId";
+export { buildFindProviders } from "./findProviders";
+export { buildInsertPatient } from "./insertPatient";
