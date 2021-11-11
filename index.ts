d09925af9928e64cc1416862285dbd32cb1cb023
eyousefifar <eyousefifar@gmail.com>
2021-11-11 17:12:39 +0330

    init admin after database initialized

--- index.ts
+++ index.ts
@@ -13,27 +13,31 @@ import {
 import { initDb as initBusinessDb, startBusinessServer } from "./app/business";
 import { initDb as initFormDb, startFormServer } from "./app/form";
 import { initDb as initNoteDb, startNoteServer } from "./app/note";
-import { initDb as initUserDb } from "./app/user";
+import { initDb as initUserDb, startUserServer } from "./app/user";
+import { initDb as initTaskDb, startTaskServer } from "./app/task";
 const app = httpClient({ logger: true });
 
 export async function startService() {
   try {
     await Promise.all([
-      await initAuthnzDb(),
-      await initAuthnzSecret(),
-      await initAdmin(),
-      await initStorageDb(),
-      await initPublicBucket(),
-      await initBusinessDb(),
-      await initFormDb(),
-      await initNoteDb(),
-      await initUserDb(),
+      initAuthnzDb(),
+      initAuthnzSecret(),
+      initStorageDb(),
+      initPublicBucket(),
+      initBusinessDb(),
+      initFormDb(),
+      initNoteDb(),
+      initUserDb(),
+      initTaskDb(),
     ]);
+    await initAdmin();
     startAuthnzServer(app);
     startStorageServer(app);
     startBusinessServer(app);
     startFormServer(app);
     startNoteServer(app);
+    startTaskServer(app);
+    startUserServer(app);
     await app.listen(3000, "0.0.0.0");
   } catch (error) {
     console.log(error);
