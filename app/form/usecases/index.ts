ff22747cb95d08d83373363400f2fa50213a59bb
eyousefifar <eyousefifar@gmail.com>
2021-11-22 18:11:46 +0330

    export retrieve mbti

--- app/form/usecases/index.ts
+++ app/form/usecases/index.ts
@@ -1,4 +1,5 @@
 import { buildRetrieveNEOPIR } from "./retrieveNEOPIR";
+import { buildRetrieveMBTI } from "./retrieveMBTI";
 
 export const retrieveNEOPIR = buildRetrieveNEOPIR();
-
+export const retrieveMBTI = buildRetrieveMBTI();
