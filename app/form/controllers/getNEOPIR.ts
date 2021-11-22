ff4d5b310c68dfcaa07eff3ff146dccdc62d656b
eyousefifar <eyousefifar@gmail.com>
2021-11-22 18:04:07 +0330

    add authentication to neo controller

--- app/form/controllers/getNEOPIR.ts
+++ app/form/controllers/getNEOPIR.ts
@@ -1,7 +1,20 @@
+import { auth, types } from "aba-node";
 import { retrieveNEOPIR } from "../usecases";
-
+import { controllerTypes } from "../types";
 export function buildGetNEOPIR() {
-  return function getNEOPIR() {
+  const roles: types.IRoles = {
+    customer: true,
+    provider: true,
+    admin: true,
+    accountant: false,
+    assistant: true,
+    support: true,
+  };
+  return function getNEOPIR(httpRequest: controllerTypes.tGetNEOPIR) {
+    const { success, error } = auth(httpRequest, roles);
+    if (!success) {
+      return error;
+    }
     return retrieveNEOPIR();
   };
 }
