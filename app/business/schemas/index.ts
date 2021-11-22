e4603dd3e107fb4b49334443a05e6f1aeb11648c
eyousefifar <eyousefifar@gmail.com>
2021-11-22 17:58:33 +0330

    remove remove request schema

--- app/business/schemas/index.ts
+++ app/business/schemas/index.ts
@@ -2,7 +2,6 @@ export { sConfirmSchema } from "./sConfirmRequest";
 export { sCreateRequest } from "./sCreateRequest";
 export { sRejectRequest } from "./sRejectRequest";
 export { sRemoveCustomer } from "./sRemoveCustomer";
-export { sRemoveRequest } from "./sRemoveRequest";
 export { sRetrieveCustomers } from "./sRetrieveCustomers";
 export { sRetrieveRequests } from "./sRetrieveRequests";
 export { sRetrieveCustomerProviderInfo } from "./sRetrieveCustomerProviderInfo";
