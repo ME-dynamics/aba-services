475e4f12d66d037d3c9cdaf6c064a867ffaec64c
eyousefifar <eyousefifar@gmail.com>
2021-11-22 17:55:33 +0330

    check for deleted requests, then return the result there should be only one request available per customer

--- app/business/adapters/db/findRequestByCustomerId.ts
+++ app/business/adapters/db/findRequestByCustomerId.ts
@@ -16,7 +16,7 @@ function selectQueryGen(): string {
 export function buildFindRequestByCustomerId(
   args: adaptersTypes.IBuildFindRequests
 ) {
-  const { select, rowToCustomers } = args;
+  const { select, rowToCustomer } = args;
   const errorPath = "business, adapters, find request by customer id";
   const query = selectQueryGen();
   return async function findRequestByCustomerId(
@@ -32,14 +32,25 @@ export function buildFindRequestByCustomerId(
     if (result.rowLength === 0) {
       return undefined;
     }
-    if(result.rowLength === 1) {
-      const customer = rowToCustomers(result.first());
-      if(customer.softDeleted || customer.requestConfirmed) {
-        return undefined
+    if (result.rowLength === 1) {
+      const customer = rowToCustomer(result.first());
+      if (customer.requestConfirmed) {
+        return undefined;
       }
       return customer;
     }
-    console.warn("customer can make only one request");
+    const requests: entityTypes.IMadeCustomersObject[] = [];
+    for (let index = 0; index < result.rowLength; index++) {
+      const row = result.rows[index];
+      if (row.get("soft_deleted")) {
+        continue;
+      }
+      requests.push(rowToCustomer(row));
+    }
+    if (requests.length === 1) {
+      return requests[0];
+    }
+    console.warn("customer can have only one provider");
     console.log(JSON.stringify(result.rows, null, 2));
     return undefined;
   };
