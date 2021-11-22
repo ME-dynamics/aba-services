4798500981dffea43fdc8d6b3c99f3a4140052df
eyousefifar <eyousefifar@gmail.com>
2021-11-22 18:07:10 +0330

    pass request to neo controller for auth

--- app/form/interfaces/retrieveNEOPIR.ts
+++ app/form/interfaces/retrieveNEOPIR.ts
@@ -1,9 +1,9 @@
 import { types } from "aba-node";
 import { getNEOPIR } from "../controllers";
-
-export function retrieveNEOPIR(request: any, reply: types.tReply) {
+import { controllerTypes } from "../types";
+export function retrieveNEOPIR(request: controllerTypes.tGetNEOPIR, reply: types.tReply) {
   try {
-    const response = getNEOPIR();
+    const response = getNEOPIR(request);
     reply.code(response.code);
     reply.send(response);
   } catch (error) {
