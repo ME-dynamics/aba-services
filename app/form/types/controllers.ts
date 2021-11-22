14d438c6d28ad2ace527d1cf27d499ce0981f0ff
eyousefifar <eyousefifar@gmail.com>
2021-11-22 18:10:30 +0330

    httpRequest controller type

--- app/form/types/controllers.ts
+++ app/form/types/controllers.ts
@@ -6,3 +6,6 @@ interface IPostSubmitFormStruct {
 }
 
 export type tPostSubmitFormStruct = types.tRequest<IPostSubmitFormStruct>;
+
+export type tGetNEOPIR = types.tRequest<unknown>;
+export type tGetMBTI = types.tRequest<unknown>;
