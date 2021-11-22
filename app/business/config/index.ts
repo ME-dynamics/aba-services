61636ae276f87f7d8b28442e4036285fea53ff5f
eyousefifar <eyousefifar@gmail.com>
2021-11-22 17:57:04 +0330

    add business strings

--- app/business/config/index.ts
+++ app/business/config/index.ts
@@ -1,16 +1,18 @@
 if (process.env.NODE_ENV !== "production") {
-    try {
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
-      const { config } = require("dotenv");
-      config();
-    } catch (error) {
-      console.log(
-        "you should install dotenv in dev when not in production environment"
-      );
-      console.log(error);
-      process.exit(1);
-    }
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const { config } = require("dotenv");
+    config();
+  } catch (error) {
+    console.log(
+      "you should install dotenv in dev when not in production environment"
+    );
+    console.log(error);
+    process.exit(1);
   }
-  
-  export const applicationName = "business";
-  export const applicationVersion = "v1";
+}
+
+export const applicationName = "business";
+export const applicationVersion = "v1";
+
+export { strings } from "./strings";
---
+++ app/business/config/strings.ts
@@ -0,0 +1,14 @@
+export const strings = {
+  requestNotFound: {
+    fa: "درخواست پیدا نشد.",
+    en: "request not found",
+  },
+  actionNotAllowed: {
+    fa: "عملیات قابل انجام نیست.",
+    en: "action not allowed",
+  },
+  userNotFound: {
+    fa: "کاربر پیدا نشد.",
+    en: "user not found",
+  },
+};
