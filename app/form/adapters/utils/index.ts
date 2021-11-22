696ecd63cd239283c5a40e46a4c2065725046db7
eyousefifar <eyousefifar@gmail.com>
2021-11-22 18:03:31 +0330

    row to form data

---
+++ app/form/adapters/utils/index.ts
@@ -0,0 +1,1 @@
+export { rowToFormData } from "./rowToFormData";
---
+++ app/form/adapters/utils/rowToFormData.ts
@@ -0,0 +1,21 @@
+import { types } from "aba-node";
+import { entityTypes } from "../../types";
+
+export function rowToFormData(
+  row: types.tRow
+): entityTypes.IMadeFormDataObject {
+  return {
+    id: row.get("id")?.toString(),
+    userId: row.get("user_id")?.toString(),
+    structureId: row.get("structure_id"),
+    formName: row.get("form_name"),
+    fields: row.get("fields"),
+    aggregates: row.get("aggregates"),
+    interpret: row.get("interpret"),
+    warnings: row.get("warnings"),
+    validation: row.get("validation"),
+    createdAt: row.get("created_at"),
+    modifiedAt: row.get("modified_at"),
+    softDeleted: row.get("soft_deleted"),
+  };
+}
