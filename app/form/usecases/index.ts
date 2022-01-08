20548b0b2d058e440c4d19fd82660f9d6a7e195a
eyousefifar <eyousefifar@gmail.com>
2022-01-08 19:13:37 +0330

    export usecases

--- app/form/usecases/index.ts
+++ app/form/usecases/index.ts
@@ -1,5 +1,17 @@
+import { insertFormData } from "../adapters";
+
 import { buildRetrieveNEOPIR } from "./retrieveNEOPIR";
 import { buildRetrieveMBTI } from "./retrieveMBTI";
+import { buildRetrieveBeckAnxiety } from "./retrieveBeckAnxiety";
+import { buildRetrieveBeckDepressionII } from "./retrieveBeckDepressionII";
+
+import { buildRetrieveTests } from "./retrieveTests";
+import { buildSubmitMBTI } from "./submitMBTI";
 
 export const retrieveNEOPIR = buildRetrieveNEOPIR();
 export const retrieveMBTI = buildRetrieveMBTI();
+export const retrieveTests = buildRetrieveTests();
+export const retrieveBeckAnxiety = buildRetrieveBeckAnxiety();
+export const retrieveBeckDepressionII = buildRetrieveBeckDepressionII();
+
+export const submitMBTI = buildSubmitMBTI({ insertFormData });
