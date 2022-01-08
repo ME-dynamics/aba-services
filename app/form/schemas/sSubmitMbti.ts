bd802623bb7afb7948cf4cdea6feb28420df5dfe
eyousefifar <eyousefifar@gmail.com>
2022-01-08 15:03:21 +0330

    fix submit mbti schema

--- app/form/schemas/sSubmitMbti.ts
+++ app/form/schemas/sSubmitMbti.ts
@@ -3,13 +3,13 @@ import { sFormData } from "./sFormData";
 function mbtiGen() {
   let schema = fluentSchema.object().maxProperties(87);
   for (let index = 1; index <= 87; index++) {
-    schema = schema.prop(`${index}`, fluentSchema.enum(["1", "2"]));
+    schema = schema.prop(`${index}`, fluentSchema.enum(["1", "2"]).required());
   }
-  return schema;
+  return fluentSchema.object().prop("fields", schema);
 }
 
 const response = {
-  [statusCodes.CREATED]: sFormData,
+  [statusCodes.CREATED]: fluentSchema.object().prop("payload", sFormData),
   [statusCodes.INTERNAL_SERVER_ERROR]: errorSchema,
 };
 
