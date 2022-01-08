e38aacdc8c7d91a6625805bc38bb8a5392a55c37
eyousefifar <eyousefifar@gmail.com>
2022-01-08 14:51:52 +0330

    neopir schema

--- app/form/schemas/sGetNEOPIR.ts
+++ app/form/schemas/sGetNEOPIR.ts
@@ -1,47 +1,13 @@
 import { errorSchema, fluentSchema, statusCodes } from "aba-node";
-
-function fieldsSchemaGen() {
-  let schema = fluentSchema.object().maxProperties(240);
-  for (let index = 1; index <= 240; index++) {
-    schema = schema.prop(
-      `${index}`,
-      fluentSchema
-        .object()
-        .prop("question", fluentSchema.string().required())
-        .prop(
-          "choices",
-          fluentSchema
-            .array()
-            .items(
-              fluentSchema
-                .object()
-                .prop("label", fluentSchema.string().required())
-                .prop(
-                  "value",
-                  fluentSchema.number().required().minimum(0).maximum(4)
-                )
-            )
-        )
-    );
-  }
-  return schema;
-}
-
-const NEOPIRStructure = fluentSchema
-  .object()
-  .prop("id", fluentSchema.string().required())
-  .prop(
-    "title",
-    fluentSchema
-      .object()
-      .prop("fa", fluentSchema.string().required())
-      .prop("en", fluentSchema.string().required())
-  )
-  .prop("description", fluentSchema.string().required())
-  .prop("fields", fieldsSchemaGen());
+import { formStructureGen } from "./sFormStructure";
 
 const response = {
-  [statusCodes.OK]: fluentSchema.object().prop("payload", NEOPIRStructure),
+  [statusCodes.OK]: fluentSchema
+    .object()
+    .prop(
+      "payload",
+      formStructureGen({ questionCount: 240, value: { min: 0, max: 4 } })
+    ),
   [statusCodes.INTERNAL_SERVER_ERROR]: errorSchema,
 };
 
