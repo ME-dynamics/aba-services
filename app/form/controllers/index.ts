9e9e33d6204df54697c57eddd4722de72e6c9375
eyousefifar <eyousefifar@gmail.com>
2022-01-28 21:27:51 +0330

    get form data controller

--- app/form/controllers/index.ts
+++ app/form/controllers/index.ts
@@ -3,7 +3,7 @@ import { buildGetTests } from "./getTests";
 import { buildGetBeckAnxiety } from "./getBeckAnxiety";
 import { buildGetBeckDepressionII } from "./getBeckDepressionII";
 import { buildGetMBTI } from "./getMBTI";
-
+import { buildGetFormData } from "./getFormData"
 import { buildPostCreateMbti } from "./postCreateMbti";
 
 export const getNEOPIR = buildGetNEOPIR();
@@ -11,5 +11,5 @@ export const getTests = buildGetTests();
 export const getBeckAnxiety = buildGetBeckAnxiety();
 export const getBeckDepressionII = buildGetBeckDepressionII();
 export const getMBTI = buildGetMBTI();
-
+export const getFormData = buildGetFormData();
 export const postCreateMbti = buildPostCreateMbti();
