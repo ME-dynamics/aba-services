29ca2561f7dac60151a670efef798f2b2bf5af05
eyousefifar <eyousefifar@gmail.com>
2022-02-15 17:49:39 +0330

    add some comment to otp gen

--- app/authnz/adapters/utils/otpGen.ts
+++ app/authnz/adapters/utils/otpGen.ts
@@ -5,9 +5,11 @@ export function buildOtpGen(args: adaptersTypes.IBuildOtpGen) {
   const errorPath = "authnz, adapters, utils, otp gen";
   return async function otpGen() {
     const otpCode = await secureRandomNumber({ min: 1e4, max: 99999 });
+    // argon2 hash
     const hashedOtpCode = await hash(`${otpCode}`);
     const otpToken = nanoid(64);
     const otpTokenValidDate = minutesFromNow(10, errorPath);
+    // one minute time to block otp request after otp is sent
     const otpTempBlockDate = minutesFromNow(1, errorPath);
     return {
       otpCode,
