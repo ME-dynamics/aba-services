83895a2aea599af0699fab50a029774d8bb63c4c
eyousefifar <eyousefifar@gmail.com>
2022-02-17 17:58:31 +0330

    add device id

--- app/authnz/controllers/postRefresh.ts
+++ app/authnz/controllers/postRefresh.ts
@@ -2,7 +2,8 @@ import { refresh } from "../usecases";
 import { controllerTypes } from "../types";
 export function buildPostRefresh() {
   return async function postRefresh(httpRequest: controllerTypes.tPostRefresh) {
-    const { userId, xJwtToken, xRefreshToken } = httpRequest.body;
-    return await refresh({ userId, xJwtToken, xRefreshToken });
+    // TODO: check refresh method for security
+    const { userId, xJwtToken, xRefreshToken, deviceId } = httpRequest.body;
+    return await refresh({ userId, xJwtToken, xRefreshToken, deviceId });
   };
 }
