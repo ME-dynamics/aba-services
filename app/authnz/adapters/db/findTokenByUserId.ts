fff6bc319f9be241c097770c98340566d2f189cc
eyousefifar <eyousefifar@gmail.com>
2022-02-17 17:47:23 +0330

    find token now uses device id as clustering key

--- app/authnz/adapters/db/findTokenByUserId.ts
+++ app/authnz/adapters/db/findTokenByUserId.ts
@@ -1,36 +1,41 @@
 import { queryGen } from "aba-node";
 import { adaptersTypes, entityTypes } from "../../types";
 
-
-
 function selectQueryGen(): string {
-    const { selectQuery, operators } = queryGen;
-    const { equal } = operators;
-    const query = selectQuery({
-        table: "token",
-        version: "v1",
-        columns: ["*"],
-        where: [equal({argument: "otp_id", dynamicValue: true})]
-    });
-    return query;
+  const { selectQuery, operators } = queryGen;
+  const { equal } = operators;
+  const query = selectQuery({
+    table: "token",
+    version: "v1",
+    columns: ["*"],
+    where: [
+      equal({ argument: "otp_id", dynamicValue: true }),
+      equal({ argument: "device_id", dynamicValue: true }),
+    ],
+  });
+  return query;
 }
 
-
-export function buildFindTokenByUserId(args: adaptersTypes.IBuildFindTokeByUserId) {
-    const { select, rowToToken } = args;
-    const errorPath = "authnz, adapters, find token by user id";
-    const query = selectQueryGen();
-    return async function findTokenByUserId(userId:string): Promise<entityTypes.IMadeTokenObject | undefined> {
-        const result = await select({
-            query,
-            params: {otp_id: userId},
-            unique: true,
-            errorPath,
-            queryOptions: undefined
-        })
-        if(result.rowLength === 0) {
-            return undefined;
-        }
-        return rowToToken(result.first());
+export function buildFindTokenByUserId(
+  args: adaptersTypes.IBuildFindTokenByUserId
+) {
+  const { select, rowToToken } = args;
+  const errorPath = "authnz, adapters, find token by user id";
+  const query = selectQueryGen();
+  return async function findTokenByUserId(
+    info: adaptersTypes.IFindTokenByUserId
+  ): Promise<entityTypes.IMadeTokenObject | undefined> {
+    const { userId, deviceId } = info;
+    const result = await select({
+      query,
+      params: { otp_id: userId, device_id: deviceId },
+      unique: false,
+      errorPath,
+      queryOptions: undefined,
+    });
+    if (result.rowLength === 0) {
+      return undefined;
     }
+    return rowToToken(result.first());
+  };
 }
