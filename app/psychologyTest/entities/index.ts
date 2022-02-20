c905295521fa5379e30fff96cefd320cc1d14a5f
eyousefifar <eyousefifar@gmail.com>
2022-02-20 17:19:21 +0330

    add new entities

--- app/psychologyTest/entities/index.ts
+++ app/psychologyTest/entities/index.ts
@@ -1,12 +1,12 @@
 import { v4 } from "uuid";
-import { buildMakeFormData } from "./formData";
-// import { aFormula } from "./NEOPIR/aFormula";
-// import { cFormula } from "./NEOPIR/cFormula";
-// import { eFormula } from "./NEOPIR/eFormula";
-// import { nFormula } from "./NEOPIR/nFormula";
-// import { oFormula } from "./NEOPIR/oFormula";
-// import { rules } from "./NEOPIR/rules";
-// import { buildMakeNEOPIR } from "./NEOPIR";
+import { buildMakeTestData } from "./testData";
+import { aFormula } from "./NEOPIR/aFormula";
+import { cFormula } from "./NEOPIR/cFormula";
+import { eFormula } from "./NEOPIR/eFormula";
+import { nFormula } from "./NEOPIR/nFormula";
+import { oFormula } from "./NEOPIR/oFormula";
+import { rules } from "./NEOPIR/rules";
+import { buildMakeNEOPIR } from "./NEOPIR";
 
 import { eiFormula } from "./MBTI/eiFormula";
 import { jpFormula } from "./MBTI/jpFormula";
@@ -14,15 +14,15 @@ import { snFormula } from "./MBTI/snFormula";
 import { tfFormula } from "./MBTI/tfFormula";
 import { buildMakeMBTI } from "./MBTI";
 
-export const makeFormData = buildMakeFormData({ uuid: v4 });
-// export const makeNEOPIR = buildMakeNEOPIR({
-//   aFormula,
-//   cFormula,
-//   eFormula,
-//   nFormula,
-//   oFormula,
-//   rules,
-// });
+export const makeTestData = buildMakeTestData({ uuid: v4 });
+export const makeNEOPIR = buildMakeNEOPIR({
+  aFormula,
+  cFormula,
+  eFormula,
+  nFormula,
+  oFormula,
+  rules,
+});
 
 export const makeMBTI = buildMakeMBTI({
   eiFormula,
