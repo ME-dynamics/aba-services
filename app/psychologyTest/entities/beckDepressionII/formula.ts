ddbbd4f4ee6653ebe44dd1db2da481c8462dba67
eyousefifar <eyousefifar@gmail.com>
2022-02-20 17:11:09 +0330

    clean up beck depression II formula

--- app/psychologyTest/entities/beckDepressionII/formula.ts
+++ app/psychologyTest/entities/beckDepressionII/formula.ts
@@ -1,26 +1,27 @@
-export function beckDepII(fields: Record<string, number>) {
-    const values = Object.values(fields);
-    let total = 0;
-    for (let index = 0; index < values.length; index++) {
-      total = total + values[index];
-    }
-    const aggregates = {
-      total,
-    };
-    const warnings: Record<string, string> = {};
-    const interpret: Record<string, string> = {};
-    if (total <= 13) {
-      interpret["depression"] = "هیچ یا کمترین";
-      if (total < 4) {
-        warnings["denial"] = "احتمال انکار افسردگی";
-      }
-    } else if (total >= 14 && total <= 19) {
-      interpret["depression"] = "افسردگی خفیف";
-    } else if (total >= 20 && total <= 28) {
-      interpret["depression"] = "افسردگی متوسط";
-    } else if (total >= 29 && total <= 63) {
-      interpret["depression"] = "افسردگی شدید";
-    }
-    return { aggregates, warnings, interpret };
+export function formula(fields: Record<string, number>) {
+  const values = Object.values(fields);
+  let total = 0;
+  for (let index = 0; index < values.length; index++) {
+    total = total + values[index];
   }
-  
+
+  if (total <= 13) {
+    const warning = total < 4 ? "اختمال انکار افسردگی" : "";
+
+    return { aggregate: total, interpret: "هیچ یا کمترین", warning };
+  }
+  if (total >= 14 && total <= 19) {
+    return { aggregate: total, interpret: "افسردگی خفیف", warning: "" };
+  }
+  if (total >= 20 && total <= 28) {
+    return { aggregate: total, interpret: "افسردگی متوسط", warning: "" };
+  }
+  if (total >= 29 && total <= 63) {
+    return { aggregate: total, interpret: "افسردگی شدید", warning: "" };
+  }
+  return {
+    aggregate: total,
+    interpret: "تعداد جواب ها نادرست است.",
+    warning: "",
+  };
+}
