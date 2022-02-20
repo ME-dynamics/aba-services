279df18fb2d4c1dbfe41e1b650d0cd2c8248fbac
eyousefifar <eyousefifar@gmail.com>
2022-02-20 17:11:31 +0330

    add beck depression II maker

--- app/psychologyTest/entities/beckDepressionII/index.ts
+++ app/psychologyTest/entities/beckDepressionII/index.ts
@@ -1,2 +1,17 @@
-export { formStructure as beckDepressionIIStructure } from "./formStructure";
-export { beckDepII } from "./formula";
+import { formula } from "./formula"; // TODO: inject formula, makes testing easier
+import type { entityTypes } from "../../types";
+
+export function buildMakeBeckDepressionII() {
+  return function makeBeckDepressionII(
+    fields: entityTypes.tBeckDepressionIIFields
+  ) {
+    const { aggregate, interpret, warning } = formula(fields);
+    return {
+      aggregate,
+      interpret,
+      warning,
+    };
+  };
+}
+
+export { testStructure as beckDepressionIIStructure } from "./testStructure";
