d562fef85e1ac4b6338961738e1ed21665398834
eyousefifar <eyousefifar@gmail.com>
2022-02-20 17:05:39 +0330

    row to test data format

--- app/psychologyTest/adapters/utils/rowToTestData.ts
+++ app/psychologyTest/adapters/utils/rowToTestData.ts
@@ -1,21 +1,19 @@
 import { types } from "aba-node";
 import { entityTypes } from "../../types";
 
-export function rowToFormData(
+export function rowToTestData(
   row: types.tRow
-): entityTypes.IMadeFormDataObject {
+): entityTypes.IMadeTestDataObject {
   return {
     id: row.get("id")?.toString(),
     userId: row.get("user_id")?.toString(),
     structureId: row.get("structure_id"),
-    formName: row.get("form_name"),
+    title: row.get("title"),
+    shortName: row.get("short_name"),
     fields: row.get("fields"),
-    aggregates: row.get("aggregates"),
-    interpret: row.get("interpret"),
-    warnings: row.get("warnings"),
-    errors: row.get("errors"),
+    results: row.get("results"),
+    resultSummary: row.get("result_summary"),
     createdAt: row.get("created_at"),
     modifiedAt: row.get("modified_at"),
-    softDeleted: row.get("soft_deleted"),
   };
 }
