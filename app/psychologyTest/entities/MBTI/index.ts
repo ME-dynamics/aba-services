26899017d3952c8fc7b65ffdcc3222944149d9f2
eyousefifar <eyousefifar@gmail.com>
2022-02-20 17:12:14 +0330

    mbti better result for new architecture

--- app/psychologyTest/entities/MBTI/index.ts
+++ app/psychologyTest/entities/MBTI/index.ts
@@ -1,55 +1,57 @@
-import { entityTypes } from "../../types";
-// FIXME: inject mbti interpret
-import { mbtiInterpret } from "./interpret";
+import type { entityTypes } from "../../types";
+
 export function buildMakeMBTI(args: entityTypes.IBuildMakeMBTI) {
   const { eiFormula, jpFormula, snFormula, tfFormula } = args;
 
   return function makeMBTI(
     fields: entityTypes.tMbtiFields
-  ): entityTypes.IMakeMbtiResult {
+  ): entityTypes.IMbtiAggregates {
     const { e, i } = eiFormula(fields);
     const { j, p } = jpFormula(fields);
     const { n, s } = snFormula(fields);
     const { f, t } = tfFormula(fields);
-    const interpret = mbtiInterpret({ e, f, i, j, n, p, s, t });
     return {
-      interpret,
-      aggregates: [
-        {
-          title: "e",
-          aggregate: e,
-        },
-        {
-          title: "i",
-          aggregate: i,
-        },
-        {
-          title: "j",
-          aggregate: j,
-        },
-        {
-          title: "p",
-          aggregate: p,
-        },
-        {
-          title: "n",
-          aggregate: n,
-        },
-        {
-          title: "s",
-          aggregate: s,
-        },
-        {
-          title: "f",
-          aggregate: f,
-        },
-        {
-          title: "t",
-          aggregate: t,
-        },
-      ],
+      e: {
+        enTitle: "Extravert",
+        faTitle: "برون گرا",
+        score: e,
+      },
+      i: {
+        enTitle: "Introvert",
+        faTitle: "درون گرا",
+        score: i,
+      },
+      s: {
+        enTitle: "Sensing",
+        faTitle: "حسی",
+        score: s,
+      },
+      n: {
+        enTitle: "Intuition",
+        faTitle: "شهودی",
+        score: n,
+      },
+      t: {
+        enTitle: "Thinking",
+        faTitle: "تفکری",
+        score: t,
+      },
+      f: {
+        enTitle: "Feeling",
+        faTitle: "احساسی",
+        score: f,
+      },
+      j: {
+        enTitle: "Judging",
+        faTitle: "داوری کننده",
+        score: j,
+      },
+      p: {
+        enTitle: "Perceiving",
+        faTitle: "ادراک کننده",
+        score: p,
+      },
     };
   };
 }
-
-export { formStructure as mbtiStructure } from "./formStructure";
+export { testStructure as mbtiStructure } from "./testStructure";
