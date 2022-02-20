93f9b25491414f13779d305fb3cc9b25334c3a4e
eyousefifar <eyousefifar@gmail.com>
2022-02-20 17:08:14 +0330

    get tests data by id

---
+++ app/psychologyTest/controllers/getTestsDataById.ts
@@ -0,0 +1,45 @@
+import { auth, types, httpResult } from "aba-node";
+// TODO: inject this method
+import { fetchCustomerProvider } from "../adapters";
+import { retrieveTestDataById } from "../usecases";
+
+import { controllerTypes } from "../types";
+
+export function buildGetTestDataById() {
+  const roles: types.IRoles = {
+    customer: true,
+    provider: true,
+    admin: true,
+    accountant: false,
+    assistant: false,
+    support: false,
+  };
+  const { badRequest, forbidden } = httpResult.clientError;
+  return async function getTestDataById(
+    httpRequest: controllerTypes.tGetTestDataById
+  ) {
+    const { success, error, payload } = auth(httpRequest, roles);
+    if (!success) {
+      return error;
+    }
+    const { id, userId } = httpRequest.params;
+    const { role } = payload;
+    if (role === "admin") {
+      if (!userId) {
+        return badRequest({ error: "user id should be defined" });
+      }
+      return await retrieveTestDataById({ testId: id, userId });
+    }
+    if (role === "provider") {
+      if (!userId) {
+        return badRequest({ error: "user id should be defined" });
+      }
+      const providerId = await fetchCustomerProvider(userId);
+      if (payload.userId !== providerId) {
+        return forbidden({ error: "action not allowed" });
+      }
+      return await retrieveTestDataById({ testId: id, userId });
+    }
+    return await retrieveTestDataById({ testId: id, userId: payload.userId });
+  };
+}
