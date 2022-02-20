8b20c7dedb930eb1005edfa7f10750f3a003bdc4
eyousefifar <eyousefifar@gmail.com>
2022-02-20 17:08:52 +0330

    export new controller

--- app/psychologyTest/controllers/index.ts
+++ app/psychologyTest/controllers/index.ts
@@ -3,7 +3,8 @@ import { buildGetTests } from "./getTests";
 import { buildGetBeckAnxiety } from "./getBeckAnxiety";
 import { buildGetBeckDepressionII } from "./getBeckDepressionII";
 import { buildGetMBTI } from "./getMBTI";
-import { buildGetFormData } from "./getFormData"
+import { buildGetTestsData } from "./getTestsData";
+import { buildGetTestDataById } from "./getTestsDataById";
 import { buildPostCreateMbti } from "./postCreateMbti";
 
 export const getNEOPIR = buildGetNEOPIR();
@@ -11,5 +12,6 @@ export const getTests = buildGetTests();
 export const getBeckAnxiety = buildGetBeckAnxiety();
 export const getBeckDepressionII = buildGetBeckDepressionII();
 export const getMBTI = buildGetMBTI();
-export const getFormData = buildGetFormData();
+export const getTestsData = buildGetTestsData();
+export const getTestDataById = buildGetTestDataById();
 export const postCreateMbti = buildPostCreateMbti();
