dee6fbea577bde11fcebbdb1e5e28df4641d3227
eyousefifar <eyousefifar@gmail.com>
2022-02-23 11:26:59 +0330

    remove comments , init log table

--- app/business/adapters/db/initDb.ts
+++ app/business/adapters/db/initDb.ts
@@ -48,87 +48,10 @@ export function buildInitDb(args: adaptersTypes.IBuildInit) {
         cluster: ["request_confirmed", "customer_id", "business_id"],
       },
     });
-    // const createProviderCustomerTable = createTableQuery({
-    //   name: "provider_customer",
-    //   version: "v1",
-    //   columns: [
-    //     { name: "provider_id", type: "UUID" },
-    //     { name: "customer_id", type: "UUID" },
-    //     { name: "name", type: "TEXT" },
-    //     { name: "profile_picture_url", type: "TEXT" },
-    //     { name: "description", type: "TEXT" },
-    //     { name: "created_at", type: "TIMESTAMP" },
-    //     { name: "modified_at", type: "TIMESTAMP" },
-    //     { name: "soft_deleted", type: "BOOLEAN" },
-    //   ],
-    //   primaryKey: {
-    //     partition: ["provider_id"],
-    //     cluster: ["created_at", "customer_id"],
-    //   },
-    //   orderBy: [{ key: "created_at", type: "DESC" }],
-    // });
-    // const customerProviderMVSelect = selectQuery({
-    //   table: "provider_customer",
-    //   version: "v1",
-    //   columns: ["*"],
-    //   where: [
-    //     notNull("provider_id"),
-    //     notNull("created_at"),
-    //     notNull("customer_id"),
-    //     notNull("soft_deleted"),
-    //   ],
-    // });
-    // const createCustomerProviderMV = createMaterialView({
-    //   name: "provider_customer_by_customer_id",
-    //   version: "v1",
-    //   selectQuery: customerProviderMVSelect,
-    //   primaryKey: {
-    //     partition: ["customer_id"],
-    //     cluster: ["soft_deleted", "created_at", "provider_id"],
-    //   },
-    // });
-
-    // const createCustomerProviderRequestTable = createTableQuery({
-    //   name: "customer_provider_request",
-    //   version: "v1",
-    //   columns: [
-    //     { name: "provider_id", type: "UUID" },
-    //     { name: "customer_id", type: "UUID" },
-    //     { name: "name", type: "TEXT" },
-    //     { name: "profile_picture_url", type: "TEXT" },
-    //     { name: "confirmed", type: "BOOLEAN" },
-    //     { name: "created_at", type: "TIMESTAMP" },
-    //     { name: "modified_at", type: "TIMESTAMP" },
-    //     { name: "soft_deleted", type: "BOOLEAN" },
-    //   ],
-    //   primaryKey: {
-    //     partition: ["provider_id"],
-    //     cluster: ["created_at", "customer_id"],
-    //   },
-    //   orderBy: [{ key: "created_at", type: "DESC" }],
-    // });
-    // const createCustomerRequestMVQuery = selectQuery({
-    //   table: "customer_provider_request",
-    //   version: "v1",
-    //   columns: ["*"],
-    //   where: [
-    //     notNull("provider_id"),
-    //     notNull("created_at"),
-    //     notNull("customer_id"),
-    //     notNull("soft_deleted"),
-    //   ],
-    // });
-    // const createCustomerRequestMV = createMaterialView({
-    //   name: "customer_provider_request_by_customer_id",
-    //   version: "v1",
-    //   selectQuery: createCustomerRequestMVQuery,
-    //   primaryKey: {
-    //     partition: ["customer_id"],
-    //     cluster: ["soft_deleted", "created_at", "provider_id"],
-    //   },
-    // });
+    
 
     await init({ query: createCustomersTable.query, errorPath });
+    await init({ query: createCustomersTable.logQuery, errorPath });
     await init({ query: createCustomersByProviderIdMV.query, errorPath });
   };
 }
