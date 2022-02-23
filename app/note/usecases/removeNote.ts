beb3cc14a9a127ff29b2abccf05f25931f353a8a
eyousefifar <eyousefifar@gmail.com>
2022-02-23 12:19:40 +0330

    fix http result, add delete note, remove note usecase

--- app/note/usecases/removeNote.ts
+++ app/note/usecases/removeNote.ts
@@ -1,11 +1,11 @@
-import { httpResultClientError, httpResultSuccess } from "aba-node";
+import { httpResult } from "aba-node";
 import { makeNote } from "../entities";
 import { usecaseTypes } from "../types";
 
 export function buildRemoveNote(args: usecaseTypes.IBuildRemoveNote) {
-  const { insertNote, findNoteById } = args;
-  const { notFound, forbidden } = httpResultClientError;
-  const { ok } = httpResultSuccess;
+  const { deleteNote, findNoteById } = args;
+  const { notFound, forbidden } = httpResult.clientError;
+  const { ok } = httpResult.success;
   return async function removeNote(info: usecaseTypes.IRemoveNote) {
     const { providerId, id } = info;
     const noteFound = await findNoteById(id);
@@ -17,8 +17,11 @@ export function buildRemoveNote(args: usecaseTypes.IBuildRemoveNote) {
       return forbidden({ error: "you do not have access to this note" });
     }
     const note = makeNote(noteFound);
-    note.set.remove();
-    await insertNote(note.object());
+    await deleteNote({
+      customerId: note.get.customerId(),
+      providerId: note.get.providerId(),
+      createdAt: note.get.createdAt(),
+    });
     return ok<string>({
       payload: "note deleted",
     });
