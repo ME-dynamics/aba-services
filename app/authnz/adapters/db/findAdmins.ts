50f0e3a9920b5aae559387f29afc679e21fd329d
eyousefifar <eyousefifar@gmail.com>
2022-02-23 11:16:15 +0330

    fix find admin query, admin equals static value true

--- app/authnz/adapters/db/findAdmins.ts
+++ app/authnz/adapters/db/findAdmins.ts
@@ -8,7 +8,7 @@ function selectQueryGen(): string {
     table: "role",
     version: "v1",
     columns: ["*"],
-    where: [equal({ argument: "admin", dynamicValue: true })],
+    where: [equal({ argument: "admin", staticValue: true })],
   });
   return query;
 }
