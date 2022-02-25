5c7811a07e6808a4eeb147bee3c1c061cd6e3099
eyousefifar <eyousefifar@gmail.com>
2022-02-25 18:03:20 +0330

    remove separate api for each test

---
+++ app/psychologyTest/usecases/retrieveTestById.ts
@@ -0,0 +1,36 @@
+import { httpResult } from "aba-node";
+import { testStructures } from "../entities";
+
+export function buildRetrieveTestById() {
+  const { ok } = httpResult.success;
+  const { notFound } = httpResult.clientError;
+  const {
+    NEOPIRStructure,
+    beckAnxietyStructure,
+    beckDepressionIIStructure,
+    garyWilsonStructure,
+    mbtiStructure,
+    mcmiStructure,
+    yemsqStructure,
+  } = testStructures;
+  return function retrieveMBTI(testId: string) {
+    switch (testId) {
+      case NEOPIRStructure.id:
+        return ok({ payload: NEOPIRStructure });
+      case beckAnxietyStructure.id:
+        return ok({ payload: beckAnxietyStructure });
+      case beckDepressionIIStructure.id:
+        return ok({ payload: beckDepressionIIStructure });
+      case garyWilsonStructure.id:
+        return ok({ payload: garyWilsonStructure });
+      case mbtiStructure.id:
+        return ok({ payload: mbtiStructure });
+      case mcmiStructure.id:
+        return ok({ payload: mcmiStructure });
+      case yemsqStructure.id:
+        return ok({ payload: yemsqStructure });
+      default:
+        return notFound({ error: "test not found" });
+    }
+  };
+}
