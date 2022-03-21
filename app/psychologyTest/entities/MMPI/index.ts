d024daf8f7c4b8e02f71c7c2f1b0d55b0fb3a899
eyousefifar <eyousefifar@gmail.com>
2022-03-21 20:37:36 +0330

    comment out mmpi, it's not usable

--- app/psychologyTest/entities/MMPI/index.ts
+++ app/psychologyTest/entities/MMPI/index.ts
@@ -1,20 +1,20 @@
-import { entityTypes } from "../../types";
+// import { entityTypes } from "../../types";
 
-export function buildMakeMBTI(args: entityTypes.IBuildMakeMBTI) {
-  const { eiFormula, jpFormula, snFormula, tfFormula } = args;
+// export function buildMakeMBTI(args: entityTypes.IBuildMakeMBTI) {
+//   const { eiFormula, jpFormula, snFormula, tfFormula } = args;
 
-  return function makeMBTI(fields: entityTypes.tMbtiFields) {
-    const eiGroup = eiFormula(fields);
-    const jpGroup = jpFormula(fields);
-    const snGroup = snFormula(fields);
-    const tfGroup = tfFormula(fields);
-    return {
-      eiGroup,
-      jpGroup,
-      snGroup,
-      tfGroup,
-    };
-  };
-}
+//   return function makeMBTI(fields: entityTypes.tMbtiFields) {
+//     const eiGroup = eiFormula(fields);
+//     const jpGroup = jpFormula(fields);
+//     const snGroup = snFormula(fields);
+//     const tfGroup = tfFormula(fields);
+//     return {
+//       eiGroup,
+//       jpGroup,
+//       snGroup,
+//       tfGroup,
+//     };
+//   };
+// }
 
-export { testStructure as mbtiStructure } from "./testStructure";
+// export { testStructure as mbtiStructure } from "./testStructure";
