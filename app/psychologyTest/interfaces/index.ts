e395e3667bb2d00a5f825a1edac03e34b1a18657
eyousefifar <eyousefifar@gmail.com>
2022-03-21 20:40:03 +0330

    change form to test

--- app/psychologyTest/interfaces/index.ts
+++ app/psychologyTest/interfaces/index.ts
@@ -6,7 +6,7 @@ import { submitTest } from "./submitTest";
 import { retrieveTestDataById } from "./retrieveTestDataById";
 import { retrieveTestHistory } from "./retrieveTestHistory";
 import { applicationVersion } from "../config";
-export async function startFormServer(app: types.tHttpInstance) {
+export async function startTestServer(app: types.tHttpInstance) {
   const routeGen = buildRouteGenerator({
     service: "tests",
     version: applicationVersion,
