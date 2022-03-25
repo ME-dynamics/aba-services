86ec3e251e5fe32c2e9d14ed5cbfccd0836d9c3d
eyousefifar <eyousefifar@gmail.com>
2022-03-25 20:36:05 +0430

    minio credentials in config

--- app/storage/config/index.ts
+++ app/storage/config/index.ts
@@ -18,3 +18,22 @@ export const scyllaContactPoint = process.env.SCYLLA_CONTACT_POINT
   ? process.env.SCYLLA_CONTACT_POINT
   : "127.0.1.1";
 export const serverUrl = "https://storage.taskyn.ir";
+// TODO: move this to vault
+
+function minioCredentialsGen() {
+  if (process.env.NODE_ENV === "production") {
+    // TODO: better validation for env vars
+    const minioAccessKey = process.env.MINIO_ACCESS_KEY || "";
+    const minioSecretKey = process.env.MINIO_SECRET_KEY || "";
+    return {
+      accessKey: minioAccessKey,
+      secretKey: minioSecretKey,
+    };
+  }
+  return {
+    accessKey: "root_user",
+    secretKey: "root_pass",
+  };
+}
+
+export const minioCredentials = minioCredentialsGen();
