9c581600663725dc5b4d4e7a517de51769cf0f54
eyousefifar <eyousefifar@gmail.com>
2022-03-28 14:46:24 +0430

    add notification keyspace, fixed naming keyspace names should match container name

--- initScyllaKeyspaces.sh
+++ initScyllaKeyspaces.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 docker exec -it taskynDb cqlsh -e "CREATE KEYSPACE authnz WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': 3 };" &&
 docker exec -it taskynDb cqlsh -e "CREATE KEYSPACE business WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': 3 };" &&
-docker exec -it taskynDb cqlsh -e "CREATE KEYSPACE notes WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': 3 };" &&
-docker exec -it taskynDb cqlsh -e "CREATE KEYSPACE tests WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': 3 };" &&
+docker exec -it taskynDb cqlsh -e "CREATE KEYSPACE note WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': 3 };" &&
+docker exec -it taskynDb cqlsh -e "CREATE KEYSPACE psychology_test WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': 3 };" &&
 docker exec -it taskynDb cqlsh -e "CREATE KEYSPACE storage WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': 3 };" &&
-docker exec -it taskynDb cqlsh -e "CREATE KEYSPACE tasks WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': 3 };" &&
-docker exec -it taskynDb cqlsh -e "CREATE KEYSPACE users WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': 3 };" &&
+docker exec -it taskynDb cqlsh -e "CREATE KEYSPACE task WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': 3 };" &&
+docker exec -it taskynDb cqlsh -e "CREATE KEYSPACE user WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': 3 };" &&
+docker exec -it taskynDb cqlsh -e "CREATE KEYSPACE notification WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': 3 };" &&
 echo "keyspaces created"
