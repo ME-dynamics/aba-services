d36dfa0a2ea4d8ef951a5a7da57d1822ffabeaff
eyousefifar <eyousefifar@gmail.com>
2022-03-29 10:37:03 +0430

    fix task keyspace naming

--- app/task/adapters/index.ts
+++ app/task/adapters/index.ts
@@ -14,7 +14,7 @@ const dbClient = scyllaClient({
   applicationVersion: "v1",
   contactPoints: ["127.0.1.1"],
   errorPath: "task, adapters, db client",
-  keyspace: "tasks",
+  keyspace: "task",
   localDataCenter: "datacenter1",
 });
 
