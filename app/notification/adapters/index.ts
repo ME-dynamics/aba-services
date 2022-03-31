27fc32ae75d6be96351a1915bbd8d6d45265bc57
eyousefifar <eyousefifar@gmail.com>
2022-03-31 12:48:08 +0430

    build and export db functions, network

--- app/notification/adapters/index.ts
+++ app/notification/adapters/index.ts
@@ -1,5 +1,13 @@
 import { scyllaClient } from "aba-node";
 
+import {
+  buildInitDb,
+  buildInsertNotification,
+  buildFindNotificationToken,
+  buildInsertNotificationToken,
+} from "./db";
+import { rowToNotificationToken } from "./utils";
+
 // TODO: move all this to config file
 const dbClient = scyllaClient({
   id: undefined,
@@ -10,3 +18,19 @@ const dbClient = scyllaClient({
   contactPoints: ["127.0.1.1"],
   errorPath: "notification, adapters",
 });
+
+export const initDb = buildInitDb({ init: dbClient.init });
+
+export const insertNotification = buildInsertNotification({
+  insert: dbClient.insert,
+});
+export const insertNotificationToken = buildInsertNotificationToken({
+  insert: dbClient.insert,
+});
+
+export const findNotificationToken = buildFindNotificationToken({
+  select: dbClient.select,
+  rowToNotificationToken,
+});
+
+export * from "./network";
