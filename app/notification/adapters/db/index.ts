d181ad7a5f526cf9b76d9d9a5c1a9efd811b91db
eyousefifar <eyousefifar@gmail.com>
2022-03-31 12:46:09 +0430

    export db functions

--- app/notification/adapters/db/index.ts
+++ app/notification/adapters/db/index.ts
@@ -1,1 +1,4 @@
-export { buildInitDb } from "./initDb"
+export { buildInitDb } from "./initDb";
+export { buildInsertNotification } from "./insertNotification";
+export { buildInsertNotificationToken } from "./insertNotificationToken";
+export { buildFindNotificationToken } from "./findNotificationToken";
