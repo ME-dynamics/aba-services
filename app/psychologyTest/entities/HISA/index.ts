c976860eccd410be2949307bb9467853611a3cc3
eyousefifar <eyousefifar@gmail.com>
2022-04-01 18:28:53 +0430

    complete the HISD test

---
+++ app/psychologyTest/entities/HISA/index.ts
@@ -0,0 +1,12 @@
+import { entityTypes } from "../../types";
+import { formula } from "./formula";
+
+export function buildMakeHISA() {
+  return function makeHISA(
+    fields: Record<string, number>
+  ): entityTypes.IHISARResult {
+    const { expressingSexualEmotions, sexualIntercourse } = formula(fields);
+
+    return { expressingSexualEmotions, sexualIntercourse };
+  };
+}
