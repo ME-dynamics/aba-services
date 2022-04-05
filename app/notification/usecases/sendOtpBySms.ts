4180d0dddab5712f0daf4c191c8d63deab98e450
eyousefifar <eyousefifar@gmail.com>
2022-04-05 11:47:38 +0430

    add provider switch algorithm

--- app/notification/usecases/sendOtpBySms.ts
+++ app/notification/usecases/sendOtpBySms.ts
@@ -1,5 +1,5 @@
 import { httpResult } from "aba-node";
-
+import { LRUMap } from "mnemonist";
 import { makeNotification } from "../entities";
 
 import type { usecaseTypes, adapterTypes, entityTypes } from "../types";
@@ -7,6 +7,16 @@ import type { usecaseTypes, adapterTypes, entityTypes } from "../types";
 export function buildSendOtpBySms(args: usecaseTypes.IBuildSendOtpSms) {
   // TODO: add multi services and random algo to choose them for sms provider
   const { insertNotification, retrieveSmsirToken, fetchSendOtpBySmsir } = args;
+  /**
+   * providerUsed is a LRU cache to store the last used provider per userId
+   * if user retries to get the sms code, another provider will be used.
+   * this hopefully increases higher chances of sms delivery
+   */
+  const providerUsed = new LRUMap<string, "smsir" | "kavenegar">(
+    String,
+    String,
+    3000
+  );
   function smsirInput(
     phoneNumber: string,
     otpCode: number,
@@ -41,15 +51,28 @@ export function buildSendOtpBySms(args: usecaseTypes.IBuildSendOtpSms) {
       modifiedAt: undefined,
     };
   }
+  function getCurrentProvider(userId: string) {
+    const lastProvider = providerUsed.get(userId);
+    if (!lastProvider) {
+      return "kavenegar";
+    }
+    // also set current provider to cache, so it can be used for next time
+    const currentProvider =
+      lastProvider === "kavenegar" ? "smsir" : "kavenegar";
+    providerUsed.set(userId, currentProvider);
+    return currentProvider;
+  }
   const { internalServerError } = httpResult.serverError;
   const { ok } = httpResult.success;
   return async function sendOtpBySms(info: usecaseTypes.ISendOtpSms) {
     const { userId, otpCode, phoneNumber } = info;
+    const currentProvider = getCurrentProvider(userId);
     const smsirToken = await retrieveSmsirToken();
     const { error, payload: token } = smsirToken;
     if (error || !token) {
       return smsirToken;
     }
+    // TODO: use current provider to send correct network request
     const { success, verificationId } = await fetchSendOtpBySmsir(
       smsirInput(phoneNumber, otpCode, token)
     );
