97582758b7e36595598bc63cef0ec19bada6a103
eyousefifar <eyousefifar@gmail.com>
2022-12-09 15:34:34 +0330

    upgrade scylla

--- runServices.dev.sh
+++ runServices.dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker run --name taskynDb -d --network host scylladb/scylla:4.5.4 --developer-mode=1 --cpuset 5 --memory 1G --listen-address 10.0.0.8 --overprovisioned 0 &&
-docker run -d --name taskynMinio --network host -e "MINIO_ROOT_USER=root_user" -e "MINIO_ROOT_PASSWORD=root_pass" --cpuset-cpus 4 --memory 256m minio/minio:RELEASE.2022-03-17T06-34-49Z server /data 
+docker run --name taskynDb -d --network host scylladb/scylla:5.0.1 --developer-mode=1 --cpuset 5 --memory 1G --overprovisioned 0 &&
+docker run -d --name taskynMinio --network host -e "MINIO_ROOT_USER=root_user" -e "MINIO_ROOT_PASSWORD=root_pass" --cpuset-cpus 4 --memory 256m minio/minio:RELEASE.2022-03-17T06-34-49Z server /data &&
 docker run --name taskynEnvoy -d --network host -v $(pwd)/envoy/envoy_node.dev.yaml:/envoy_node.yaml -v $(pwd)/envoy/certs/taskyn-ir.crt:/taskyn-ir.crt -v $(pwd)/envoy/certs/taskyn-ir.key:/taskyn-ir.key --memory 128mb --cpuset-cpus 3 envoyproxy/envoy-distroless:v1.21.1 -c /envoy_node.yaml
